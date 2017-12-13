7140eb16701ee9260338110accbfbe822466213e
PlanB <city7310@naver.com>
2017-12-13 15:32:53 +0900

    [DONE] Configs

---
+++ Server-Student/config/production.py
@@ -0,0 +1,13 @@
+import socket
+
+from config import *
+
+HOST = socket.gethostbyname(socket.gethostname())
+ENDPOINT = '{0}:{1}'.format(HOST, PORT)
+SWAGGER.update({'host': ENDPOINT})
+
+DEBUG = False
+
+MONGODB_SETTINGS = {
+    'db': '{0}-production'.format(SERVICE_NAME)
+}
