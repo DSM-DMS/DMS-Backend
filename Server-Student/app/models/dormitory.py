29989f5da005ac7af915b68c48de6e6205d7fbd5
PlanB <city7310@naver.com>
2017-12-14 19:26:53 +0900

    Initialize

--- Server-Student/app/models/dormitory.py
+++
@@ -1,9 +0,0 @@
-from app.models import *
-from app.models.account import StudentModel
-
-
-class ReportModel(Document):
-    author = ReferenceField(StudentModel, required=True)
-    title = StringField(required=True)
-    room = IntField(required=True)
-    content = StringField(required=True)
