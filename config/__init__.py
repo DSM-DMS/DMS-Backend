8453d7880225fa278e5f92ad15f07c535fed2998
PlanB <city7310@naver.com>
2017-12-14 22:58:32 +0900

    [UPDATE] Enhance configs
    - Add JWT properties
    - Add TEST property

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,10 @@ PORT = 3000
 SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
 # Secret key for any 3-rd party libraries
 
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=365)
+JWT_HEADER_TYPE = 'JWT'
+
 SERVICE_NAME = 'DMS'
 
 SWAGGER = {
