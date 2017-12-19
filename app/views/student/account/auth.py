a86be5d2e848f870d04de5467fca4da2a1609061
PlanB <city7310@naver.com>
2017-12-19 11:02:11 +0900

    [DONE] Student Auth API

---
+++ app/views/student/account/auth.py
@@ -0,0 +1,49 @@
+from binascii import hexlify
+from hashlib import pbkdf2_hmac
+from uuid import uuid4
+
+from flask import Response, current_app
+from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_refresh_token_required
+from flask_restful import Resource, request
+from flasgger import swag_from
+
+from app.models.account import SignupWaitingModel, StudentModel, RefreshTokenModel
+
+
+class Auth(Resource):
+    def post(self):
+        """
+        학생 로그인
+        """
+        id = request.form['id']
+        pw = request.form['pw']
+
+        encrypted_pw = hexlify(
+            data=pbkdf2_hmac(
+                hash_name='sha256',
+                password=pw.encode(),
+                salt=current_app.secret_key.encode(),
+                iterations=100000
+            )
+        ).decode('utf-8')
+
+        student = StudentModel.objects(
+            id=id,
+            pw=encrypted_pw
+        ).first()
+
+        if student:
+            return Response('', 401)
+
+        refresh_token = uuid4()
+        RefreshTokenModel(
+            token=refresh_token,
+            token_owner=student,
+            pw_snapshot=encrypted_pw
+        ).save()
+
+        return {
+            'access_token': create_access_token(id),
+            'refresh_token': create_refresh_token(str(refresh_token))
+        }, 200
