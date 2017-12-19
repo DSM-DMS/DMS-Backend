40369dcd69581156d14f34da5ae7a60c30255233
PlanB <city7310@naver.com>
2017-12-19 09:43:44 +0900

    [UPDATE] Add docstring, meta, change field declaration to facility_report.py

--- app/models/facility_report.py
+++ app/models/facility_report.py
@@ -5,9 +5,33 @@ from app.models.account import StudentModel
 
 
 class FacilityReportModel(Document):
-    informant = ReferenceField(StudentModel)
-    title = StringField(required=True)
-    room = IntField(required=True)
-    content = StringField(required=True)
+    """
+    Facility report document
+    """
+    meta = {
+        'collection': 'facility_report'
+    }
 
-    report_date = StringField(required=True, default=str(date.today()))
+    report_date = DateTimeField(
+        required=True,
+        default=date.today()
+    )
+
+    informant = ReferenceField(
+        document_type=StudentModel,
+        required=True
+    )
+
+    room = IntField(
+        required=True,
+        min_value=200,
+        max_value=599
+    )
+
+    title = StringField(
+        required=True
+    )
+
+    content = StringField(
+        required=True
+    )
