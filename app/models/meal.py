af4233831d2de124eec510893e79996918ab7b52
PlanB <city7310@naver.com>
2017-12-19 09:43:48 +0900

    [UPDATE] Add docstring, meta, change field declaration to meal.py

--- app/models/meal.py
+++ app/models/meal.py
@@ -2,7 +2,25 @@ from app.models import *
 
 
 class MealModel(Document):
-    date = StringField(primary_key=True)
-    breakfast = ListField(required=True)
-    lunch = ListField(required=True)
-    dinner = ListField(required=True)
+    """
+    School's meal document
+    """
+    meta = {
+        'collection': 'meal'
+    }
+
+    date = StringField(
+        primary_key=True
+    )
+
+    breakfast = ListField(
+        required=True
+    )
+
+    lunch = ListField(
+        required=True
+    )
+
+    dinner = ListField(
+        required=True
+    )
