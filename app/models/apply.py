971d74b796f5216b7a274c5373796e81cdad66e0
PlanB <city7310@naver.com>
2017-12-19 09:43:30 +0900

    [UPDATE] Add docstring, meta, change field declaration to apply.py

--- app/models/apply.py
+++ app/models/apply.py
@@ -4,23 +4,76 @@ from app.models import *
 
 
 class ApplyBase(EmbeddedDocument):
-    apply_date = StringField(required=True, default=str(date.today()))
-    meta = {'allow_inheritance': True}
+    """
+    Apply data of student base document
+    """
+    meta = {
+        'allow_inheritance': True
+    }
+
+    apply_date = DateTimeField(
+        required=True,
+        default=date.today()
+    )
 
 
-class AfterSchoolApplyModel(ApplyBase):
-    applied = ListField(StringField())
+# class AfterSchoolApplyModel(ApplyBase):
+#     applied = ListField(
+#         StringField(
+#             required=True
+#         )
+#     )
 
 
 class ExtensionApplyModel(ApplyBase):
-    class_ = IntField(required=True)
-    seat = IntField(required=True)
+    """
+    Extension apply data of student document includes 11st, 12nd extension apply
+    """
+    meta = {
+        'collection': 'extension_apply'
+    }
+
+    class_ = IntField(
+        required=True
+    )
+
+    seat = IntField(
+        required=True
+    )
 
 
 class GoingoutApplyModel(ApplyBase):
-    on_saturday = BooleanField(required=True, default=False)
-    on_sunday = BooleanField(required=True, default=False)
+    """
+    Goingout apply data of student document
+    """
+    meta = {
+        'collection': 'goingout_apply'
+    }
+
+    on_saturday = BooleanField(
+        required=True,
+        default=False
+    )
+
+    on_sunday = BooleanField(
+        required=True,
+        default=False
+    )
 
 
 class StayApplyModel(ApplyBase):
-    value = IntField(required=True, default=4)
+    """
+    Stay apply data of student document
+    1 : Friday homecoming
+    2 : Saturday homecoming
+    3 : Saturday dormitory coming
+    4 : Stay
+    """
+    meta = {
+        'collection': 'stay_apply'
+    }
+
+    value = IntField(
+        required=True,
+        default=4
+    )
