f0ba1cf6abd9d1f3e25bee199d85fa36f47c59e2
PlanB <city7310@naver.com>
2017-12-27 08:44:11 +0900

    [DONE] Admin Extension API Swagger

---
+++ Server/app/docs/admin/apply/extension.py
@@ -0,0 +1,21 @@
+EXTENSION_GET = {
+    'tags': ['신청 정보 관리'],
+    'description': '연장신청 정보 다운로드',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '연장신청 정보 추출 성공. 엑셀 파일 응답'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
