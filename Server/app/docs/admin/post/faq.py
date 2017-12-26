c31066a99c9a3c2bea72a55ce87d905f59e9d08b
PlanB <city7310@naver.com>
2017-12-27 08:44:57 +0900

    [DONE] Admin FAQ API & API Swagger

---
+++ Server/app/docs/admin/post/faq.py
@@ -0,0 +1,113 @@
+FAQ_POST = {
+    'tags': ['게시글 관리'],
+    'description': 'FAQ 업로드',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'title',
+            'description': 'FAQ 제목',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'content',
+            'description': 'FAQ 내용',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': 'FAQ 업로드 성공'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
+
+FAQ_PATCH = {
+    'tags': ['게시글 관리'],
+    'description': 'FAQ 수정',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'id',
+            'description': '수정할 FAQ ID',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'title',
+            'description': 'FAQ 제목',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'content',
+            'description': 'FAQ 내용',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '수정 성공'
+        },
+        '204': {
+            'description': '존재하지 않는 게시글'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
+
+FAQ_DELETE = {
+    'tags': ['게시글 관리'],
+    'description': 'FAQ 삭제',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'id',
+            'description': '삭제할 FAQ ID',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '삭제 성공'
+        },
+        '204': {
+            'description': '존재하지 않는 게시글'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
---
+++ Server/app/views/admin/post/faq.py
@@ -0,0 +1,88 @@
+from flask import Response
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_restful import Resource, request
+from flasgger import swag_from
+
+from app.docs.admin.post.faq import *
+from app.models.account import AdminModel
+from app.models.post import FAQModel
+
+
+class AdminFAQ(Resource):
+    @swag_from(FAQ_POST)
+    @jwt_required
+    def post(self):
+        """
+        FAQ 업로드
+        """
+        admin = AdminModel.objects(
+            id=get_jwt_identity()
+        ).first()
+        if not admin:
+            return Response('', 403)
+
+        title = request.form['title']
+        content = request.form['content']
+
+        FAQModel(
+            author=admin,
+            title=title,
+            content=content
+        ).save()
+
+        return Response('', 201)
+
+    @swag_from(FAQ_PATCH)
+    @jwt_required
+    def patch(self):
+        """
+        FAQ 수정
+        """
+        admin = AdminModel.objects(
+            id=get_jwt_identity()
+        ).first()
+        if not admin:
+            return Response('', 403)
+
+        id = request.form['id']
+        title = request.form['title']
+        content = request.form['content']
+
+        faq = FAQModel.objects(
+            id=id
+        ).first()
+
+        if not faq:
+            return Response('', 204)
+
+        faq.update(
+            title=title,
+            content=content
+        )
+
+        return Response('', 200)
+
+    @swag_from(FAQ_DELETE)
+    @jwt_required
+    def delete(self):
+        """
+        FAQ 제거
+        """
+        admin = AdminModel.objects(
+            id=get_jwt_identity()
+        ).first()
+        if not admin:
+            return Response('', 403)
+
+        id = request.form['id']
+
+        faq = FAQModel.objects(
+            id=id
+        ).first()
+
+        if not faq:
+            return Response('', 204)
+
+        faq.delete()
+
+        return Response('', 200)
