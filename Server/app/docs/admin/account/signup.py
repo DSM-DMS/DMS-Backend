d39f1b03d300f3b1b994bc08f90878b61d7eb552
PlanB <city7310@naver.com>
2017-12-26 17:17:46 +0900

    [UPDATE] Admin New Account API Swagger

---
+++ Server/app/docs/admin/account/signup.py
@@ -0,0 +1,45 @@
+NEW_ACCOUNT_POST = {
+    'tags': ['관리자 계정'],
+    'description': '관리자 계정 생성',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'id',
+            'description': '생성할 관리자 계정 ID',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'pw',
+            'description': '생성할 관리자 계정 PW',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'name',
+            'description': '생성활 관리자 계정 이름',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': '계정 생성 성공'
+        },
+        '204': {
+            'description': '계정 생성 실패(이미 존재하는 ID)'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
