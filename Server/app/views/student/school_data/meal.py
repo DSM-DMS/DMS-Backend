9ec9257eee2247546d23609adb8aeba8542dbdfe
PlanB <city7310@naver.com>
2017-12-26 10:10:02 +0900

    [DONE] Meal API

---
+++ Server/app/views/student/school_data/meal.py
@@ -0,0 +1,42 @@
+import json
+
+from flask import Response
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_restful import Resource, request
+from flasgger import swag_from
+
+from app.models.account import StudentModel
+from app.models.meal import MealModel
+
+
+class Meal(Resource):
+    @jwt_required
+    def get(self):
+        student = StudentModel.objects(
+            id=get_jwt_identity()
+        ).first()
+
+        if not student:
+            return Response('', 403)
+
+        date = request.args['date']
+
+        meal = MealModel.objects(
+            date=date
+        ).first()
+
+        if not meal:
+            return Response('', 204)
+
+        return Response(
+            json.dumps(
+                {
+                    'breakfast': meal.breakfast,
+                    'lunch': meal.lunch,
+                    'dinner': meal.dinner
+                },
+                ensure_ascii=False
+            ),
+            200,
+            content_type='application/json; charset=utf8'
+        )
