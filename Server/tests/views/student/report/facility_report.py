488571d24ed6940568c04235f23a6562517286d6
PlanB <city7310@naver.com>
2017-12-27 13:15:26 +0900

    [DONE] Student facility report API TC

---
+++ Server/tests/views/student/report/facility_report.py
@@ -0,0 +1,56 @@
+import json
+import unittest2 as unittest
+
+from app.models.account import AdminModel, StudentModel
+from app.models.report import FacilityReportModel
+from tests.views import admin, student
+
+from server import app
+
+
+class TestFacilityReport(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        admin.create_fake_account()
+        student.create_fake_account()
+
+        self.student_access_token = student.get_access_token(self.client)
+        self.admin_access_token = admin.get_access_token(self.client)
+
+    def tearDown(self):
+        StudentModel.objects(
+            id='fake'
+        ).delete()
+
+        AdminModel.objects(
+            id='fake'
+        ).delete()
+
+        FacilityReportModel.objects(
+            title='test',
+            content='test',
+            room=415
+        ).delete()
+
+    def testReport(self):
+        rv = self.client.post('/report/facility')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.post('/report/facility', headers={'Authorization': self.student_access_token}, data={'title': 'test', 'content': 'test', 'room': 415})
+        self.assertEqual(rv.status_code, 201)
+        # Report success
+
+        rv = self.client.get('/admin/report/facility')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.get('/admin/report/facility', headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 200)
+
+        flag = False
+        for report in json.loads(rv.data):
+            if report['title'] == report['content'] == 'test' and report['room'] == 415:
+                flag = True
+
+        self.assertTrue(flag)
