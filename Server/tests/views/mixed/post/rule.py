e710e7018f2070a475a21ee0f45050cb07e73b31
PlanB <city7310@naver.com>
2017-12-28 08:41:05 +0900

    [DONE] Rule API TC

---
+++ Server/tests/views/mixed/post/rule.py
@@ -0,0 +1,117 @@
+import json
+import unittest2 as unittest
+
+from tests.views import admin, student
+
+from server import app
+
+
+class TestRule(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        admin.create_fake_account()
+        student.create_fake_account()
+
+        self.admin_access_token = admin.get_access_token(self.client)
+
+    def tearDown(self):
+        student.remove_fake_account()
+        admin.remove_fake_account()
+
+    def testA_post(self):
+        """
+        TC about rule upload
+
+        1. Check 'unauthorized on rule upload'
+        2. Check 'upload succeed'
+        """
+        rv = self.client.post('/admin/rule')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.post('/admin/rule', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
+        self.assertEqual(rv.status_code, 201)
+        # Post success
+
+    def testB_get(self):
+        """
+        TC about rule get
+        """
+
+    def testC_patch(self):
+        """
+        TC about rule patch
+
+        1. Check 'unauthorized on rule getting'
+        2. Load/Check 'existing(uploaded) rule'
+        3. Check 'unauthorized on rule patch'
+        4. Check 'patch succeed'
+        """
+        rv = self.client.get('/rule')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.get('/rule', headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 200)
+        # Get success
+
+        data = json.loads(rv.data.decode())
+
+        post_id = ''
+        for rule in data:
+            if rule['title'] == 'test' and rule['author'] == 'fake':
+                post_id = rule['id']
+
+        self.assertTrue(post_id)
+        # Data validate
+
+        rv = self.client.patch('/admin/rule')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.patch('/admin/rule', headers={'Authorization': self.admin_access_token}, data={'id': '123456789012345678901234', 'title': 'new', 'content': 'new'})
+        self.assertEqual(rv.status_code, 204)
+        # Post id doesn't exist
+
+        rv = self.client.patch('/admin/rule', headers={'Authorization': self.admin_access_token}, data={'id': post_id, 'title': 'new', 'content': 'new'})
+        self.assertEqual(rv.status_code, 200)
+        # Patch success
+
+    def testD_delete(self):
+        """
+        TC about rule delete
+
+        1. Check 'unauthorized on rule getting'
+        2. Load/Check 'existing(uploaded) rule'
+        3. Check 'unauthorized on rule delete'
+        4. Check 'delete succeed'
+        """
+        rv = self.client.get('/rule')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.get('/rule', headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 200)
+        # Get success
+
+        data = json.loads(rv.data.decode())
+
+        post_id = ''
+        for rule in data:
+            if rule['title'] == 'new' and rule['author'] == 'fake':
+                post_id = rule['id']
+
+        self.assertTrue(post_id)
+        # Data validate
+
+        rv = self.client.delete('/admin/rule')
+        self.assertEqual(rv.status_code, 401)
+        # Unauthorized check
+
+        rv = self.client.delete('/admin/rule', headers={'Authorization': self.admin_access_token}, data={'id': '123456789012345678901234'})
+        self.assertEqual(rv.status_code, 204)
+        # Post id doesn't exist
+
+        rv = self.client.delete('/admin/rule', headers={'Authorization': self.admin_access_token}, data={'id': post_id})
+        self.assertEqual(rv.status_code, 200)
+        # Delete success
