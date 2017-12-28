2550f6e3dac918ea135ea663c57af3c13cc045ce
PlanB <city7310@naver.com>
2017-12-28 17:02:28 +0900

    [DONE] Admin point rule delete API & Swagger, patch swagger contents

--- Server/app/docs/admin/point/rule.py
+++ Server/app/docs/admin/point/rule.py
@@ -67,7 +67,7 @@ POINT_RULE_MANAGING_POST = {
             'in': 'formData',
             'type': 'int',
             'required': True
-        },
+        }
     ],
     'responses': {
         '201': {
@@ -117,11 +117,46 @@ POINT_RULE_MANAGING_PATCH = {
             'in': 'formData',
             'type': 'int',
             'required': True
-        },
+        }
     ],
     'responses': {
-        '201': {
-            'description': '상벌점 데이터 등록 성공'
+        '200': {
+            'description': '상벌점 규칙 수정 성공'
+        },
+        '204': {
+            'description': '존재하지 않는 규칙 ID'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
+
+POINT_RULE_MANAGING_DELETE = {
+    'tags': ['상벌점 관리'],
+    'description': '상벌점 규칙 삭제',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'rule_id',
+            'description': '삭제할 상벌점 규칙 ID',
+            'in': 'formData',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '상벌점 규칙 삭제 성공'
+        },
+        '204': {
+            'description': '존재하지 않는 규칙 ID'
         },
         '403': {
             'description': '권한 없음'
