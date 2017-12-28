ab85c1afd09e5995b195dca25df017d23f99e525
Sangmin <sm372181@gmail.com>
2017-12-28 15:38:40 +0900

    [FIX] test

--- Server/tests/views/account_admin/account_control.py
+++ Server/tests/views/account_admin/account_control.py
@@ -55,5 +55,5 @@ class TestAccountControl(unittest.TestCase):
 
         uuid = SignupWaitingModel.objects(number=1234).first().uuid
         response = json.loads(rv.data.decode())
-        self.assertEqual(uuid, response['uuid'])
+        self.assertEqual(uuid, response['UUID'])
         # Validate
