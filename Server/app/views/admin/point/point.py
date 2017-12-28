0e6f52d8a94d4ce320ccaf460315ff95d93d52f1
PlanB <city7310@naver.com>
2017-12-28 10:54:56 +0900

    [DONE] Admin point managing API

--- Server/app/views/admin/point/point.py
+++ Server/app/views/admin/point/point.py
@@ -63,12 +63,51 @@ class PointManaging(Resource):
         """
         특정 학생의 상벌점 내역 조회
         """
+        admin = AdminModel.objects(id=get_jwt_identity()).first()
+        if not admin:
+            return Response('', 403)
+
+        id = request.form['id']
+        student = StudentModel.objects(id=id).first()
+        if not student:
+            return Response('', 204)
+
+        response = [{
+            'time': str(history.time)[:-7],
+            'reason': history.reason.name,
+            'point': history.point
+        } for history in student.point_histories]
+
+        return Response(json.dumps(response, ensure_ascii=False), 200, content_type='application/json; charset=utf8')
 
     @jwt_required
     def post(self):
         """
         특정 학생에 대한 상벌점 부여
         """
+        admin = AdminModel.objects(id=get_jwt_identity()).first()
+        if not admin:
+            return Response('', 403)
+
+        id = request.form['id']
+        student = StudentModel.objects(id=id).first()
+        if not student:
+            return Response('', 204)
+
+        reason_id = request.form['reason_id']
+        reason = PointRuleModel.objects(id=reason_id).first()
+        if not reason:
+            return Response('', 205)
+
+        point = int(request.form['point'])
+
+        student.point_histories.append(PointHistoryModel(
+            reason=reason,
+            point=point
+        ))
+        student.save()
+
+        return Response('', 201)
 
 
 class PointRuleManaging(Resource):
