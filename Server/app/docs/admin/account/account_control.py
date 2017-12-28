3e0b0123629db04904800d70d73ad1a18838829e
Sangmin <sm372181@gmail.com>
2017-12-28 15:12:01 +0900

    [REFACTOR] Account control

--- Server/app/docs/admin/account/account_control.py
+++ Server/app/docs/admin/account/account_control.py
@@ -18,48 +18,19 @@ ACCOUNT_CONTROL_POST = {
         }
     ],
     'responses': {
-        '200': {
-            'description': '계정 삭제 성공',
-            },
-        '204': {
-            'description': '해당 학번에 해당하는 계정 없음'
-        },
-        '403': {
-            'description': '권한 없음'
-        }
-    }
-}
-
-ACCOUNT_CONTROL_GET = {
-    'tags': ['관리자 계정'],
-    'description': 'UUID 받아오기',
-    'parameters': [
-        {
-            'name': 'Authorization',
-            'description': 'JWT Token',
-            'in': 'header',
-            'type': 'str',
-            'required': True
-        },
-        {
-            'name': 'number',
-            'description': '학번',
-            'in': 'formData',
-            'type': 'int',
-            'required': True
-        }
-    ],
-    'responses': {
-        '200': {
-            'description': '성공',
+        '201': {
+            'description': '계정 삭제, UUID 생성 성공',
             'examples': {
                 'application/json': {
-                    'UUID': '022d'
+                    'UUID':  '022d'
                 }
             }
         },
         '204': {
-            'description': '이미 가입된 학번'
+            'description': '해당 학번에 해당하는 계정, UUID 없음'
+        },
+        '401': {
+            'description': 'JWT Token 없음'
         },
         '403': {
             'description': '권한 없음'
