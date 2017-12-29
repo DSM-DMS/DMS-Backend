42d54e0b1f896a33def10d4150cebf62ae221733
Sangmin <sm372181@gmail.com>
2017-12-29 13:43:16 +0900

    [UPDATE] delete exit()

--- Server/server.py
+++ Server/server.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     if app.config['TEST']:
         all_tests = ut.TestLoader().discover('tests', pattern='*.py')
         ut.TextTestRunner().run(all_tests)
-        exit()
     else:
         from utils import db_migrator
 
