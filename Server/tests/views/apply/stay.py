c27c545cb71013da64e77a90dd3d9563dce42757
PlanB <city7310@naver.com>
2018-01-01 22:54:08 +0900

    [DONE] Admin stay API TC

--- Server/tests/views/apply/stay.py
+++ Server/tests/views/apply/stay.py
@@ -2,7 +2,7 @@ from datetime import datetime, time
 import json
 import unittest2 as unittest
 
-from tests.views import account_student
+from tests.views import account_admin, account_student
 
 from server import app
 
@@ -11,10 +11,13 @@ class TestStay(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
+        account_admin.create_fake_account()
         account_student.create_fake_account()
-        self.access_token = account_student.get_access_token(self.client)
+        self.admin_access_token = account_admin.get_access_token(self.client)
+        self.student_access_token = account_student.get_access_token(self.client)
 
     def tearDown(self):
+        account_admin.remove_fake_account()
         account_student.remove_fake_account()
 
     def testA_apply(self):
@@ -25,19 +28,26 @@ class TestStay(unittest.TestCase):
         2. Check 'apply succeed'
         3. Check 'apply status'
         """
-        rv = self.client.get('/stay', headers={'Authorization': self.access_token})
+        rv = self.client.get('/stay', headers={'Authorization': self.student_access_token})
         self.assertEqual(rv.status_code, 200)
         self.assertEqual(json.loads(rv.data.decode())['value'], 4)
 
-        rv = self.client.post('/stay', headers={'Authorization': self.access_token}, data={'value': 1})
+        rv = self.client.post('/stay', headers={'Authorization': self.student_access_token}, data={'value': 1})
         now = datetime.now()
         if (now.weekday() == 6 and now.time() > time(20, 30)) or (0 < now.weekday() < 3) or (now.weekday() == 3 and now.time() < time(22, 00)):
             self.assertEqual(rv.status_code, 201)
             # Apply success
 
-            rv = self.client.get('/stay', headers={'Authorization': self.access_token})
+            rv = self.client.get('/stay', headers={'Authorization': self.student_access_token})
             self.assertEqual(rv.status_code, 200)
 
             self.assertEqual(json.loads(rv.data.decode())['value'], 1)
         else:
             self.assertEqual(rv.status_code, 204)
+
+    def testB_download(self):
+        """
+        TC about download stay apply
+        """
+        rv = self.client.get('/admin/stay', headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 200)
