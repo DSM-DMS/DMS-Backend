98b5b19d9280a641de96a6b7505d522d8a2591b8
PlanB <city7310@naver.com>
2018-01-01 22:30:27 +0900

    [UPDATE] Add argument parser for travis-ci

--- Server/server.py
+++ Server/server.py
@@ -1,9 +1,14 @@
+from argparse import ArgumentParser
 import os
 import unittest2 as ut
 
 from app import app
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-t', '--travis', required=False)
+    args = parser.parse_args()
+
     if app.config['TEST']:
         all_tests = ut.TestLoader().discover('tests', pattern='*.py')
         ut.TextTestRunner().run(all_tests)
@@ -13,6 +18,5 @@ if __name__ == '__main__':
         if os.getenv('MYSQL_PW'):
             db_migrator.migrate_posts()
 
+    if not args.travis:
         app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.debug, threaded=True)
-
-    # app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.debug, threaded=True)
