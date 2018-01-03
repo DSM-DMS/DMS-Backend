b6bd542bdd36a500e9d68fee9967bada67ae0174
PlanB <city7310@naver.com>
2018-01-03 19:19:37 +0900

    [UPDATE] Improve FAQ API TC structre

--- Server/tests/views/post/faq.py
+++ Server/tests/views/post/faq.py
@@ -3,6 +3,7 @@ import unittest2 as unittest
 
 from tests.views import account_admin, account_student
 
+from app.models.post import FAQModel
 from server import app
 
 
@@ -12,111 +13,205 @@ class TestFAQ(unittest.TestCase):
 
         account_admin.create_fake_account()
         account_student.create_fake_account()
-
         self.admin_access_token = account_admin.get_access_token(self.client)
+        self.student_access_token = account_student.get_access_token(self.client)
 
     def tearDown(self):
         account_admin.remove_fake_account()
         account_student.remove_fake_account()
+        FAQModel.objects.delete()
 
     def testA_post(self):
         """
-        TC about FAQ upload
+        TC about FAQ uploading
 
-        1. Check 'upload succeed'
+        - Preparations
+        None
+
+        - Exception Tests
+        Forbidden with student access token
+
+        - Process
+        Post FAQ data
+
+        - Validation
+        Check post data is existing
         """
+        # -- Preparations --
+        # -- Preparations --
+
+        # -- Exception Tests --
+        rv = self.client.post('/admin/faq', headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 403)
+        # -- Exception Tests --
+
+        # -- Process --
         rv = self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
         self.assertEqual(rv.status_code, 201)
-        # Post success
+        # -- Process --
+
+        # -- Validation --
+        rv = self.client.get('/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue(json.loads(rv.data.decode()))
+        # -- Validation --
 
     def testB_get(self):
         """
-        TC about FAQ get
-        1. Check short wrong id
-        2. Check wrong id
-        3. Success
+        TC about FAQ data getting
+
+        - Preparations
+        Post sample FAQ data
+        Take FAQ ID
+
+        - Exception Tests
+        Short FAQ ID with admin/student access token
+        Non-existing FAQ ID with admin/student access token
+
+        - Process
+        Load FAQ data with admin access token
+        Load FAQ data with student access token
+
+        - Validation
+        None
         """
-        self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
+        # -- Preparations --
+        rv = self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
+        self.assertEqual(rv.status_code, 201)
+        # -- Preparations --
 
-        rv = self.client.get('/faq', headers={'Authorization': self.admin_access_token})
-        rule_id = json.loads(rv.data.decode())[0]['id']
-        # Make rule and get rule id
-
+        # -- Exception Tests --
         rv = self.client.get('/faq/1234', headers={'Authorization': self.admin_access_token})
         self.assertEqual(rv.status_code, 204)
-        # Check short wrong id
+
+        rv = self.client.get('/faq/1234', headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 204)
 
         rv = self.client.get('/faq/123456789012345678901234', headers={'Authorization': self.admin_access_token})
         self.assertEqual(rv.status_code, 204)
-        # Check wrong id
 
-        rv = self.client.get('/faq/'+rule_id, headers={'Authorization': self.admin_access_token})
+        rv = self.client.get('/faq/123456789012345678901234', headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 204)
+        # -- Exception Tests --
+
+        # -- Process --
+        rv = self.client.get('/faq', headers={'Authorization': self.admin_access_token})
         self.assertEqual(rv.status_code, 200)
-        # Success
+        faq_id = json.loads(rv.data.decode())[0]['id']
+
+        rv = self.client.get('/faq/' + faq_id, headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue(json.loads(rv.data.decode()))
+
+        rv = self.client.get('/faq', headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 200)
+        faq_id = json.loads(rv.data.decode())[0]['id']
+
+        rv = self.client.get('/faq/' + faq_id, headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue(json.loads(rv.data.decode()))
+        # -- Process --
+
+        # -- Validation --
+        # -- Validation --
 
     def testC_patch(self):
         """
-        TC about FAQ patch
+        TC about FAQ data patching
 
-        1. Load/Check 'existing(uploaded) FAQ'
-        2. Check 'unauthorized on FAQ patch'
-        3. Check 'patch succeed'
+        - Preparations
+        Post sample FAQ data
+        Take FAQ ID
+
+        - Exception Tests
+        Forbidden with student access token
+        Short FAQ ID
+        Non-existing FAQ ID
+
+        - Process
+        Change FAQ data
+
+        - Validation
+        Check changed FAQ data
         """
-        self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
+        # -- Preparations --
+        rv = self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
+        self.assertEqual(rv.status_code, 201)
 
         rv = self.client.get('/faq', headers={'Authorization': self.admin_access_token})
         self.assertEqual(rv.status_code, 200)
-        # Get success
+        faq_id = json.loads(rv.data.decode())[0]['id']
+        # -- Preparations --
 
-        data = json.loads(rv.data.decode())
+        # -- Exception Tests --
+        rv = self.client.patch('/admin/faq', headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 403)
 
-        post_id = ''
-        for faq in data:
-            if faq['title'] == 'test' and faq['author'] == 'fake':
-                post_id = faq['id']
-
-        self.assertTrue(post_id)
-        # Data validate
+        rv = self.client.patch('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': '1234', 'title': 'new', 'content': 'new'})
+        self.assertEqual(rv.status_code, 204)
 
         rv = self.client.patch('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': '123456789012345678901234', 'title': 'new', 'content': 'new'})
         self.assertEqual(rv.status_code, 204)
-        # Post id doesn't exist
+        # -- Exception Tests --
 
-        rv = self.client.patch('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': post_id, 'title': 'new', 'content': 'new'})
+        # -- Process --
+        rv = self.client.patch('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': faq_id, 'title': 'new', 'content': 'new'})
         self.assertEqual(rv.status_code, 200)
-        # Patch success
+        # -- Process --
+
+        # -- Validation --
+        rv = self.client.get('/faq/' + faq_id, headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 200)
+
+        data = json.loads(rv.data.decode())
+        self.assertTrue(data['title'] == data['content'] == 'new')
+        # -- Validation --
 
     def testD_delete(self):
         """
-        TC about FAQ delete
+        TC about FAQ data deletion
 
-        1. Load/Check 'existing(uploaded) FAQ'
-        2. Check 'delete succeed'
+        - Preparations
+        Post sample FAQ data
+        Take FAQ ID
+
+        - Exception Tests
+        Forbidden with student access token
+        Short FAQ ID
+        Non-existing FAQ ID
+
+        - Process
+        Delete FAQ data
+
+        - Validation
+        Check FAQ data is empty
         """
-        self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
-
+        # -- Preparations --
         rv = self.client.post('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'title': 'test', 'content': 'test'})
         self.assertEqual(rv.status_code, 201)
-        # Post success
 
         rv = self.client.get('/faq', headers={'Authorization': self.admin_access_token})
         self.assertEqual(rv.status_code, 200)
-        # Get success
+        faq_id = json.loads(rv.data.decode())[0]['id']
+        # -- Preparations --
 
-        data = json.loads(rv.data.decode())
+        # -- Exception Tests --
+        rv = self.client.delete('/admin/faq', headers={'Authorization': self.student_access_token})
+        self.assertEqual(rv.status_code, 403)
 
-        post_id = ''
-        for faq in data:
-            if faq['title'] == 'test' and faq['author'] == 'fake':
-                post_id = faq['id']
-
-        self.assertTrue(post_id)
-        # Data validate
-
-        rv = self.client.delete('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': '123456789012345678901234'})
+        rv = self.client.delete('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': '1234', 'title': 'new', 'content': 'new'})
         self.assertEqual(rv.status_code, 204)
-        # Post id doesn't exist
 
-        rv = self.client.delete('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': post_id})
+        rv = self.client.delete('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': '123456789012345678901234', 'title': 'new', 'content': 'new'})
+        self.assertEqual(rv.status_code, 204)
+        # -- Exception Tests --
+
+        # -- Process --
+        rv = self.client.delete('/admin/faq', headers={'Authorization': self.admin_access_token}, data={'id': faq_id})
         self.assertEqual(rv.status_code, 200)
-        # Delete success
+        # -- Process --
+
+        # -- Validation --
+        rv = self.client.get('/faq/' + faq_id, headers={'Authorization': self.admin_access_token})
+        self.assertEqual(rv.status_code, 204)
+        # -- Validation --
