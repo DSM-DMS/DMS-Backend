40d1a0c961ab74a7fba9444dca3c2109e8ead7df
JoMingyu <city7310@naver.com>
2018-02-05 15:37:02 +0900

    [UPDATE] Move uuid_excel_save function to UUIDExcelToDB class

--- Server/app/views/system/uuid_excel_to_db.py
+++ Server/app/views/system/uuid_excel_to_db.py
@@ -1,8 +1,10 @@
+from openpyxl import load_workbook
+
 from flask import Blueprint, Response
 from flask_restful import Api
 
+from app.models.account import SignupWaitingModel
 from app.views import BaseResource
-from utils.uuid_excel import uuid_excel_save
 
 api = Api(Blueprint('uuid-excel-to-db-api', __name__))
 api.prefix = '/system'
@@ -10,6 +12,18 @@ api.prefix = '/system'
 
 @api.resource('/uuid-excel-to-db')
 class UUIDExcelToDB(BaseResource):
+    def _uuid_excel_save(self):
+        for i in range(1, 4):
+            for j in range(1, 5):
+                wb = load_workbook('uuid_{0}_{1}.xlsx'.format(i, j)).get_sheet_by_name('Sheet1')
+                for k in range(2, 23):
+                    if wb['A{0}'.format(k)].value:
+                        SignupWaitingModel(
+                            uuid=wb['C{0}'.format(k)].value,
+                            name=wb['A{0}'.format(k)].value,
+                            number=int(wb['B{0}'.format(k)].value)
+                        ).save()
+
     def get(self):
-        uuid_excel_save()
+        self._uuid_excel_save()
         return Response('', 201)
