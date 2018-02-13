78e89b01fbb6f0c73fa7093d72999aad0b389f09
JoMingyu <city7310@naver.com>
2018-02-13 11:46:26 +0900

    [DONE] Test, deploy apply status cleaning scheduler, move template routings

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flasgger import Swagger
@@ -30,6 +30,16 @@ def create_app(dev=True):
 
 
 app = create_app(False)
+
+
+@app.route('/')
+def student():
+    return render_template('student.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 
 @app.after_request
--- Server/server.py
+++ Server/server.py
@@ -1,29 +1,23 @@
-from argparse import ArgumentParser
-import os
-
-from flask import render_template
-
 from app import app
 
 
-@app.route('/')
-def student():
-    return render_template('student.html')
-
-
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
-
-
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+
     parser = ArgumentParser()
     parser.add_argument('-p', '--port')
     args = parser.parse_args()
+
+    import os
+    if 'SECRET_KEY' not in os.environ:
+        print('[WARN] SECRET KEY is not set in the environment variable !!')
 
     if os.getenv('MYSQL_PW'):
         from utils import db_migrator
 
         db_migrator.migrate_posts()
+
+    from utils.schedulers import apply_status_cleaner
+    apply_status_cleaner.run()
 
     app.run(host=app.config['HOST'], port=int(args.port) if args.port else app.config['PORT'], debug=app.debug, threaded=True)
