55a68c31e02c6e941c1bfb446c98304e1cd97708
JoMingyu <city7310@naver.com>
2018-02-13 11:31:03 +0900

    [UPDATE] Remove temporary auth's logic

--- Server/app/views/student/account/auth.py
+++ Server/app/views/student/account/auth.py
@@ -36,14 +36,7 @@ class Auth(BaseResource):
         student = StudentModel.objects(id=id, pw=pw).first()
 
         if not student:
-            student = StudentModel.objects(id=id).first()
-            if not student:
-                abort(401)
-            else:
-                # 비밀번호 암호화가 어떤 key로 salting되었는지 모르므로 id만 같다면 일단 pass시켜 줌
-                # 새롭게 암호화된 비밀번호로 업데이트
-                # TODO 비밀번호 암호화 관련 이슈를 해결하기 위한 것이므로 추후 제거해야 함
-                student.update(pw=pw)
+            abort(401)
 
         # --- Auth success
 
