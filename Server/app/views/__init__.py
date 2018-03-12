29f52aa9f32e24a10bf995a71ef379d8ad20524b
JoMingyu <city7310@naver.com>
2018-03-12 11:52:54 +0900

    ADD: Comments on BaseResource

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -11,11 +11,25 @@ from app.models.account import AdminModel, StudentModel, SystemModel
 
 
 class BaseResource(Resource):
+    """
+    BaseResource with some helper functions based flask_restful.Resource
+    """
     def __init__(self):
         self.now = time.strftime('%Y-%m-%d %H:%M:%S')
 
     @classmethod
     def unicode_safe_json_response(cls, data, status_code=200):
+        """
+        Helper function which processes json response with unicode using ujson
+        - About ujson.dumps(data, ensure_ascii=False)
+        If ensure_ascii is true (the default),
+        all non-ASCII characters in the output are escaped with \\uXXXX sequences,
+        and the result is a str instance consisting of ASCII characters only.
+        If ensure_ascii is false, some chunks written to fp may be unicode instances.
+        :type data: dict or list
+        :type status_code: int
+        :rtype: Response
+        """
         return Response(
             ujson.dumps(data, ensure_ascii=False),
             status_code,
