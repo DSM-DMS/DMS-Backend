93feea74a37233c2696a1188de042264adedac90
JoMingyu <city7310@naver.com>
2018-03-12 11:39:57 +0900

    UPDATE: Extend JWT access token's expire to 30 days

--- Server/config/__init__.py
+++ Server/config/__init__.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
     # Secret key for any 3-rd party libraries
 
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=365)
     JWT_HEADER_TYPE = 'JWT'
 
