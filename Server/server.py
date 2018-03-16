e4c1df6d81284f32ca603569d3e4546420d65c9b
JoMingyu <city7310@naver.com>
2018-03-16 12:26:54 +0900

    DEBUG : Print slack bot token when server started

--- Server/server.py
+++ Server/server.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     if 'SECRET_KEY' not in os.environ:
         print('[WARN] SECRET KEY is not set in the environment variable !!')
 
+    print(app.config['SLACK_BOT_TOKEN'])
+
     # if os.getenv('MYSQL_PW'):
     #     from utils import db_migrator
     #
