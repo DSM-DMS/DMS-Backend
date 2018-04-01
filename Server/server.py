407d2c77b723c7609caf27cbc82038467606d38f
JoMingyu <city7310@naver.com>
2018-04-01 19:29:10 +0900

    UPDATE: 서버 실행 시 급식 파싱

--- Server/server.py
+++ Server/server.py
@@ -12,12 +12,10 @@ if __name__ == '__main__':
     if 'SECRET_KEY' not in os.environ:
         print('[WARN] SECRET KEY is not set in the environment variable !!')
 
-    # if os.getenv('MYSQL_PW'):
-    #     from utils import db_migrator
-    #
-    #     db_migrator.migrate_posts()
-
     from utils.schedulers import apply_status_cleaner
     apply_status_cleaner.run()
+
+    from utils.meal_parser import parse
+    parse()
 
     app.run(host=app.config['HOST'], port=int(args.port) if args.port else app.config['PORT'], debug=app.debug, threaded=True)
