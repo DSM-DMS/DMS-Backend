9b25693c878681d72fcb3c42c4ebb8d3ade9ccff
JoMingyu <city7310@naver.com>
2018-04-08 21:56:02 +0900

    ADD: config_v2

---
+++ Server/config_v2/dev.py
@@ -0,0 +1,16 @@
+from config_v2 import Config
+
+
+class DevConfig(Config):
+    HOST = 'localhost'
+
+    if not Config.REPRESENTATIVE_HOST:
+        Config.SWAGGER['host'] = '{}:{}'.format(HOST, Config.PORT)
+
+    DEBUG = True
+
+    MONGODB_SETTINGS = {
+        'host': 'localhost',
+        'port': 27017,
+        'db': '{}-dev'.format(Config.SERVICE_NAME)
+    }
