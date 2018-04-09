747c18a30512d479fc14a761bf4808f74a7d169a
RISMME <python@istruly.sexy>
2018-04-09 22:26:35 +0900

    [UPDATE] database migrator

--- Server/utils/db_migrator.py
+++ Server/utils/db_migrator.py
@@ -1,55 +1,125 @@
-# import os
-#
-# import pymysql
-# from pymysql.cursors import DictCursor
-#
-# from app_v1.models.account import StudentModel
-# from app_v1.models.apply import GoingoutApplyModel, StayApplyModel
-# from app_v1.models.post import FAQModel, NoticeModel, RuleModel
-#
-# connection = pymysql.connect(
-#     host='localhost',
-#     user='root',
-#     password=os.getenv('MYSQL_PW'),
-#     db='dsm_dms',
-#     charset='utf8'
-# )
-#
-#
-# def migrate_apply(id, uuid):
-#     student = StudentModel.objects(id=id).first()
-#
-#     cursor = connection.cursor(DictCursor)
-#
-#     cursor.execute("SELECT * FROM goingout_apply WHERE uid='{0}'".format(uuid))
-#     result = cursor.fetchall()
-#     goingout_apply = GoingoutApplyModel(on_saturday=result[0]['sat'], on_sunday=result[0]['sun']) if result else None
-#
-#     cursor.execute("SELECT * FROM stay_apply WHERE uid='{0}'".format(uuid))
-#     result = cursor.fetchall()
-#     stay_apply = StayApplyModel(value=result[0]['value']) if result else None
-#
-#     student.update(goingout_apply=goingout_apply, stay_apply=stay_apply)
-#
-#
-# def migrate_posts():
-#     FAQModel.drop_collection()
-#     NoticeModel.drop_collection()
-#     RuleModel.drop_collection()
-#
-#     cursor = connection.cursor(DictCursor)
-#
-#     cursor.execute('SELECT * FROM faq')
-#     faqs = cursor.fetchall()
-#     for faq in faqs:
-#         FAQModel(title=faq['title'], content=faq['content'], author='사감실').save()
-#
-#     cursor.execute('SELECT * FROM notice')
-#     notices = cursor.fetchall()
-#     for notice in notices:
-#         NoticeModel(title=notice['title'], content=notice['content'], author='사감실').save()
-#
-#     cursor.execute('SELECT * FROM rule')
-#     rules = cursor.fetchall()
-#     for rule in rules:
-#         RuleModel(title=rule['title'], content=rule['content'], author='사감실').save()
+from pymongo import MongoClient
+
+from app_v2.models.account import *
+from app_v2.models.point import *
+from app_v2.models.post import *
+from app_v2.models.report import *
+from app_v2.models.version import *
+
+client = MongoClient()
+db = client['dms']
+
+
+def _migration_account():
+    student = db['student_model']
+    for student_account in student.find():
+        StudentModel(
+            signup_time=student_account['signup_time'],
+            id=student_account['id'],
+            pw=student_account['pw'],
+            name=student_account['name'],
+
+            number=student_account['number'],
+            good_point=student_account['good_point'],
+            bad_point=student_account['bad_point'],
+            point_histories=[
+                PointHistoryModel(
+                    id=history['id'],
+                    time=history['time'],
+                    reason=history['reason'],
+                    point_type=history['point'],
+                    point=history['point']
+                ) for history in student_account['point_history']
+            ],
+            penalty_training_status=student_account['penalty_training_status'],
+            penalty_level=student_account['penalty_level']
+        ).save()
+
+    admin = db['admin_model']
+    for admin_account in admin.find():
+        AdminModel(
+            signup_time=admin_account['signup_time'],
+            id=admin_account['id'],
+            pw=admin_account['pw'],
+            name=admin_account['name']
+        ).save()
+
+
+def _migration_point_rule():
+    point_rules = db['point_rule']
+    for rule in point_rules:
+        PointRuleModel(
+            name=rule['name'],
+            point_type=rule['point_type'],
+            min_point=rule['min_point'],
+            max_point=rule['max_point']
+        ).save()
+
+
+def _migration_post():
+    FAQs = db['faq']
+    notices = db['notice']
+    rules = db['rule']
+
+    for FAQ in FAQs:
+        FAQModel(
+            write_time=FAQ['write_time'],
+            author=FAQ['author'],
+            title=FAQ['title'],
+            content=FAQ['content'],
+            pinned=FAQ['pinned']
+        ).save()
+
+    for notice in notices:
+        NoticeModel(
+            write_time=notice['write_time'],
+            author=notice['author'],
+            title=notice['title'],
+            content=notice['content'],
+            pinned=notice['pinned']
+        ).save()
+
+    for rule in rules:
+        RuleModel(
+            write_time=rule['write_time'],
+            author=rule['author'],
+            title=rule['title'],
+            content=rule['content'],
+            pinned=rule['pinned']
+        ).save()
+
+
+def _migration_report():
+    facilities = db['facility_report']
+    for facility in facilities:
+        FacilityReportModel(
+            report_time=facility['report_time'],
+            author=facility['author'],
+            title=facility['title'],
+            content=facility['content'],
+            room=facility['room']
+        ).save()
+
+
+def _migration_version():
+    versions = db['version']
+    for version in versions:
+        if version['platform'] == 'Android':
+            platform = 2
+        elif version['platform'] == 'IOS':
+            platform = 3
+        else:
+            platform = 1
+
+        VersionModel(
+            platform=platform,
+            version=version['version']
+        ).save()
+
+
+def migration():
+    _migration_account()
+    _migration_point_rule()
+    _migration_post()
+    _migration_report()
+    _migration_version()
