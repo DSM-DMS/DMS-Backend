a0a8a5e77293b469bc05ca539c9198a6ec6e8cef
JoMingyu <city7310@naver.com>
2018-04-11 08:50:17 +0900

    REMOVE: bug report 관련
    UPDATE: 버그 리포트에서 데이터베이스에 내용을 저장하지 않도록 함

--- Server/app_v1/views/student/report/bug_report.py
+++ Server/app_v1/views/student/report/bug_report.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from slacker import Slacker
 
-from flask import Blueprint, current_app, g, request
+from flask import Blueprint, Response, current_app, g, request
 from flask_restful import Api
 from flasgger import swag_from
 
@@ -9,7 +9,6 @@ from app_v1.views import BaseResource
 from app_v1.views import student_only
 
 from app_v1.docs.student.report.bug_report import *
-from app_v2.models.report import BugReportModel
 
 api = Api(Blueprint('student-bug-report-api', __name__))
 
@@ -22,6 +21,8 @@ class BugReport(BaseResource):
             2: 'Android',
             3: 'iOS'
         }
+        
+        super(BugReport, self).__init__()
 
     @swag_from(BUG_REPORT_POST)
     @student_only
@@ -35,13 +36,9 @@ class BugReport(BaseResource):
         platform_type = int(request.form['platform_type'])
         time = datetime.now()
 
-        report = BugReportModel(author=student.name, content=content, platform_type=platform_type, report_time=time).save()
-
         if not current_app.testing:
             slack_bot = Slacker(current_app.config['SLACK_BOT_TOKEN'])
             slack_bot.chat.post_message(channel='#bug-report', text='제보자: {}\n제보시간: {}\n플랫폼: {}\n내용: {}'
                                         .format(student.name, str(time)[:-7], self.PLATFORM_TYPES[platform_type], content))
 
-        return self.unicode_safe_json_response({
-            'id': str(report.id)
-        }, 201)
+        return Response('', 201)
