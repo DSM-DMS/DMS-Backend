aa360c9338fa2d55b58e213eedd550deae12d3ad
RISMME <python@istruly.sexy>
2018-04-15 18:18:48 +0900

    DONE: Student report docs v2

---
+++ Server/app/docs/v2/student/report/facility_report.py
@@ -0,0 +1,40 @@
+FACILITY_REPORT_POST = {
+    'tags': ['신고'],
+    'description': '시설고장신고',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'content',
+            'description': '시설고장신고 내용',
+            'in': 'json',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'room',
+            'description': '호실 번호',
+            'in': 'json',
+            'type': 'int',
+            'required': True
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': '시설고장 신고 성공. 업로드된 시설고장 신고의 ID 응답',
+            'examples': {
+                'application/json': {
+                    'id': '13211265df16ads'
+                }
+            }
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
