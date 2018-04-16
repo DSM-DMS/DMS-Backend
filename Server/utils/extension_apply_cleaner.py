69dcde05b73726c86eae3fd0b858aba4ef2139e4
JoMingyu <city7310@naver.com>
2018-04-16 11:50:05 +0900

    FIX: Apply cleaner

--- Server/utils/extension_apply_cleaner.py
+++ Server/utils/extension_apply_cleaner.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 import time
 
-from app.models.v2.apply import ExtensionApplyBase
+from app.models.v2.apply import ExtensionApply11Model, ExtensionApply12Model
 
 
 def run():
     while True:
-        if datetime.now().time().hour == 0:
-            ExtensionApplyBase.objects.delete()
-            time.sleep(1800)
+        if 0 < datetime.now().time().hour < 12:
+            ExtensionApply11Model.objects.delete()
+            ExtensionApply12Model.objects.delete()
+            time.sleep(3600)
