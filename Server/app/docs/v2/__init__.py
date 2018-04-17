b40112389b3ad90c70ee4e9228d9e26f28459cdc
JoMingyu <city7310@naver.com>
2018-04-17 11:34:54 +0900

    ADD: Sample Access Token, Refresh Token 추가, 관리자 계정 tag 추가

--- Server/app/docs/v2/__init__.py
+++ Server/app/docs/v2/__init__.py
@@ -4,6 +4,16 @@ SAMPLE_OBJECT_IDS = [
     '5acddc2bc2a93f68ce96f5c9',
     '5acddc2bc2a93f68ce96f5ce'
 ]
+SAMPLE_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' \
+                      'eyJmcmVzaCI6ZmFsc2UsImlkZW50aXR5IjoiYSIsInR5cGUiOiJhY2Nlc3MiLCJleHAiOjE1NDA1NT' \
+                      'c0NDYsImp0aSI6ImJiN2M3MjJmLTZkZjMtNDljYy1iZTk5LWRkMjMzNDU1NDRjZSIsIm5iZiI6MTUwOTA' \
+                      'yMTQ0NiwiaWF0IjoxNTA5MDIxNDQ2fQ.' \
+                      'wmytxSuQlH-KjhxO2EzrIioWHWgEnyiqWpRBwWuM15M'
+SAMPLE_REFRESH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.' \
+                       'eyJleHAiOjE1MTM5MDQ3NjMsImlkZW50aXR5IjoibmlsIiwiZnJlc2giOmZhbHNlLCJqdGkiOiI1Y' \
+                       'zg1ZDAxNy1lYjIwLTRmYjgtYmVhYi1iYmYyZTQyY2NlYmYiLCJuYmYiOjE1MTM2NDU1NjMsInR5cGU' \
+                       'iOiJhY2Nlc3MiLCJpYXQiOjE1MTM2NDU1NjN9.' \
+                       '075C0_-b-oqSWc-jz7G35y00erRVntpcqN9uMIAnvfI'
 
 TEMPLATE = {
     'schemes': [
@@ -13,6 +23,10 @@ TEMPLATE = {
         {
             'name': '계정 관리',
             'description': '관리자 권한으로 접근 가능한 계정/계정 관리 API'
+        },
+        {
+            'name': '관리자 계정',
+            'description': '관리자 계정 관련 API'
         }
     ]
 }
