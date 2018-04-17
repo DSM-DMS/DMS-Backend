91a98a6a1627e81bfd8075f7742db3c6024fcd59
JoMingyu <city7310@naver.com>
2018-04-18 08:51:47 +0900

    ADD: Admin 게시글 프리뷰 API Swagger doc

---
+++ Server/app/docs/v2/admin/post/preview.py
@@ -0,0 +1,37 @@
+def get_preview_post_doc(type):
+    return {
+        'tags': ['게시글 관리'],
+        'description': '{} 프리뷰를 설정합니다.'.format(type),
+        'parameters': [
+            {
+                'name': 'Authorization',
+                'description': 'JWT Token',
+                'in': 'header',
+                'type': 'str',
+                'required': True
+            },
+            {
+                'name': 'id',
+                'description': '프리뷰를 설정할 {}의 ID'.format(type),
+                'in': 'json',
+                'type': 'str',
+                'required': True
+            }
+        ],
+        'responses': {
+            '201': {
+                'description': '설정 성공'
+            },
+            '204': {
+                'description': '존재하지 않는 게시글 ID'
+            },
+            '403': {
+                'description': '권한 없음'
+            }
+        }
+    }
+
+
+FAQ_PREVIEW_POST = get_preview_post_doc('FAQ')
+NOTICE_PREVIEW_POST = get_preview_post_doc('공지사항')
+RULE_PREVIEW_POST = get_preview_post_doc('기숙사규정')
