48061dbb9dd98b40dff2fdcdf04605b3bdc34de7
JoMingyu <city7310@naver.com>
2018-04-17 11:06:38 +0900

    ADD: 계정 관리 태그, Sample UUID, ObjectID

--- Server/app/docs/v2/__init__.py
+++ Server/app/docs/v2/__init__.py
@@ -1,11 +1,18 @@
+SAMPLE_UUID = '3e04'
+SAMPLE_OBJECT_IDS = [
+    '5acddc2bc2a93f68ce96f5c4',
+    '5acddc2bc2a93f68ce96f5c9',
+    '5acddc2bc2a93f68ce96f5ce'
+]
+
 TEMPLATE = {
     'schemes': [
         'http'
     ],
     'tags': [
         {
-            'name': 'Some Tag',
-            'description': 'Some API'
-        },
+            'name': '계정 관리',
+            'description': '관리자 권한으로 접근 가능한 계정/계정 관리 API'
+        }
     ]
 }
