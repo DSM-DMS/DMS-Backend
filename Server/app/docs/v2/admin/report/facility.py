4d09d6db58d3e507edc09ce66221970c7c7a4f40
JoMingyu <city7310@naver.com>
2018-04-18 08:52:16 +0900

    ADD: Admin 시설고장 신고 API Swagger doc

---
+++ Server/app/docs/v2/admin/report/facility.py
@@ -0,0 +1,82 @@
+from app.docs.v2 import SAMPLE_OBJECT_IDS
+
+
+FACILITY_REPORT_GET = {
+    'tags': ['신고 정보 관리'],
+    'description': '시설고장 신고 정보를 조회합니다.',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '시설고장 신고 리스트 조회 성공',
+            'examples': {
+                '': [
+                    {
+                        'id': SAMPLE_OBJECT_IDS[0],
+                        'author': '이병찬',
+                        'title': '방에서 냄새가 나요',
+                        'content': '내 발냄새인거같기도 하고',
+                        'room': 415
+                    },
+                    {
+                        'id': SAMPLE_OBJECT_IDS[1],
+                        'author': '조민규',
+                        'title': '계엄령을 선포한다',
+                        'content': '치킨먹고싶어',
+                        'room': 415
+                    },
+                    {
+                        'id': SAMPLE_OBJECT_IDS[2],
+                        'author': '김동규',
+                        'title': '피카츄가 포켓볼에서 안나와요',
+                        'content': '내가 이걸 얼마주고 샀는데',
+                        'room': 320
+                    }
+                ]
+            }
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
+
+
+FACILITY_REPORT_DELETE = {
+    'tags': ['신고 정보 관리'],
+    'description': '시설고장 신고 정보를 삭제합니다.',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'reportId',
+            'description': '삭제할 시설고장 신고 ID',
+            'in': 'json',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '시설고장신고 삭제 성공'
+        },
+        '204': {
+            'description': '존재하지 않는 시설고장 신고 ID'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
