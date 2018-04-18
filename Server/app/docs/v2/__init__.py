768ca8d7786b565b63c567ccc17ed9798d5ba448
JoMingyu <city7310@naver.com>
2018-04-18 12:12:02 +0900

    UPDATE: Swagger doc v1 TEMPLATE
    - Admin과 Mixed section을 주석으로 나눔
    - Mixed section에 태그 추가

--- Server/app/docs/v2/__init__.py
+++ Server/app/docs/v2/__init__.py
@@ -20,6 +20,7 @@ TEMPLATE = {
         'http'
     ],
     'tags': [
+        # --- Admin
         {
             'name': '계정 관리',
             'description': '관리자 권한으로 접근 가능한 계정/계정 관리 API'
@@ -47,6 +48,26 @@ TEMPLATE = {
         {
             'name': '설문지 괸리',
             'description': '관리자 권한으로 접근 가능한 설문지 관리 API'
+        },
+        # --- Admin
+
+        # --- Mixed
+        {
+            'name': 'JWT',
+            'description': '로그인 상태 체크, Access token refresh 등 JWT 관련 API'
+        },
+        {
+            'name': '메타데이터',
+            'description': 'DMS에 들어갈 Github/Facebook 링크, 개발자 목록 등 메타데이터용 API'
+        },
+        {
+            'name': '게시글',
+            'description': '학생 권한으로 접근 가능한 게시글 API'
+        },
+        {
+            'name': '학교 정보',
+            'description': '학교 정보 API'
         }
+        # --- Mixed
     ]
 }
