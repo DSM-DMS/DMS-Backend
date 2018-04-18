cd5bbf0fe2fa72e493f7c5b677ad95ec5c0990e3
JoMingyu <city7310@naver.com>
2018-04-18 11:52:28 +0900

    UPDATE: Swagger doc v1 TEMPLATE를 docs/에서 docs/v1/으로 이동

---
+++ Server/app/docs/v1/__init__.py
@@ -0,0 +1,75 @@
+TEMPLATE = {
+    'schemes': [
+        'http'
+    ],
+    'tags': [
+        {
+            'name': '관리자 계정',
+            'description': '관리자 권한으로 접근 가능한 계정/계정 관리 API'
+        },
+        {
+            'name': '계정',
+            'description': '학생 권한으로 접근 가능한 계정 API'
+        },
+
+        # {
+        #     'name': '방과후 신청 관리',
+        #     'description': '관리자 권한으로 접근 가능한 방과후 신청 관리 API'
+        # },
+        # {
+        #     'name': '방과후 신청',
+        #     'description': '학생 권한으로 접근 가능한 방과후 신청 API'
+        # },
+
+        {
+            'name': '신청 정보 관리',
+            'description': '관리자 권한으로 접근 가능한 신청 정보 관리 API'
+        },
+        {
+            'name': '신청',
+            'description': '학생 권한으로 접근 가능한 신청 API'
+        },
+
+        {
+            'name': '신고 관리',
+            'description': '관리자 권한으로 접근 가능한 시설 고장, DMS 시스템 등 신고 관리 API'
+        },
+        {
+            'name': '신고',
+            'description': '학생 권한으로 접근 가능한 시설 고장, DMS 시스템 등 신고 API'
+        },
+
+        {
+            'name': '게시글 관리',
+            'description': '관리자 권한으로 접근 가능한 게시글 관리 API'
+        },
+        {
+            'name': '게시글',
+            'description': '학생 권한으로 접근 가능한 게시글 조회 관련 API'
+        },
+
+        {
+            'name': '설문지 관리',
+            'description': '관리자 권한으로 접근 가능한 설문지 관리 API'
+        },
+        {
+            'name': '설문지',
+            'description': '학생 권한으로 접근 가능한 설문지 API'
+        },
+
+        {
+            'name': '상벌점 관리',
+            'description': '관리자 권한으로 접근 가능한 상벌점 관리 API'
+        },
+
+        {
+            'name': '학교',
+            'description': '학생과 관리자 권한으로 접근 가능한 학교 정보 조회 관련 API'
+        },
+
+        {
+            'name': '버전',
+            'description': '플랫폼 최신 버전 관련 api'
+        }
+    ]
+}
