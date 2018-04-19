accdb956f35b8df78e9b333ef12d03efa1121709
JoMingyu <city7310@naver.com>
2018-04-19 14:16:37 +0900

    ADD: Post preview get API swagger doc

---
+++ Server/app/docs/v2/mixed/post/preview.py
@@ -0,0 +1,30 @@
+def generate_preview_get_doc(type):
+    return {
+        'tags': ['게시글', '게시글 관리'],
+        'description': '{} 프리뷰를 조회합니다. 별도로 프리뷰로 설정된 게시글이 없으면 가장 최신 글을 불러옵니다.'.format(type),
+        'responses': {
+            '200': {
+                'description': '프리뷰 조회 성공',
+                'examples': {
+                    '': {
+                        'writeTime': '2017-10-11',
+                        'author': '교촌치킨',
+                        'title': '치킨 먹는법',
+                        'content': '교촌허니콤보를 웨지감자와 함께',
+                        'pinned': True
+                    }
+                }
+            },
+            '204': {
+                'description': '게시글이 하나도 없어 반환할 정보가 없음'
+            },
+            '403': {
+                'description': '권한 없음'
+            }
+        }
+    }
+
+
+FAQ_PREVIEW_GET = generate_preview_get_doc('FAQ')
+NOTICE_PREVIEW_GET = generate_preview_get_doc('공지사항')
+RULE_PREVIEW_GET = generate_preview_get_doc('기숙사규정')
