ce378bb87d15f82890b915aa4741fe432ca3ee83
JoMingyu <city7310@naver.com>
2018-04-19 11:39:54 +0900

    DONE: Auth check API swagger doc

---
+++ Server/app/docs/v2/mixed/jwt/checker.py
@@ -0,0 +1,21 @@
+AUTH_CHECK_GET = {
+    'tags': ['JWT'],
+    'description': '해당 Access Token이 유효한지(\'로그인되어 있음\'으로 표현할 수 있는지) 체크합니다.',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+    ],
+    'responses': {
+        '200': {
+            'description': 'Access Token이 유효함',
+        },
+        '204': {
+            'description': 'Access Token이 유효하지 않음'
+        }
+    }
+}
