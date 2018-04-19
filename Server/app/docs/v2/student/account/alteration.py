01e03931b12afe34268a40de72cce849c5b11c1f
JoMingyu <city7310@naver.com>
2018-04-19 14:33:05 +0900

    UPDATE: Student delete account API를 alteration으로 이동

--- Server/app/docs/v2/student/account/alteration.py
+++ Server/app/docs/v2/student/account/alteration.py
@@ -33,3 +33,39 @@ CHANGE_PW_POST = {
         }
     }
 }
+
+DELETE_ACCOUNT_DELETE = {
+    'tags': ['계정'],
+    'description': '학생 자신의 계정을 삭제합니다.',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'id',
+            'description': '현재 아이디',
+            'in': 'json',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'password',
+            'description': '현재 비밀번호',
+            'in': 'json',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '계정 삭제 성공'
+        },
+        '403': {
+            'description': '비밀번호 변경 실패(틀린 ID나 비밀번호), 또는 권한 없음'
+        }
+    }
+}
