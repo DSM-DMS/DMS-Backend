40c32bda5d6eda39fef0649fcda03c54f70c2249
JoMingyu <city7310@naver.com>
2018-04-19 14:16:08 +0900

    ADD: Link API swagger doc

---
+++ Server/app/docs/v2/mixed/metadata/links.py
@@ -0,0 +1,24 @@
+LINKS_GET = {
+    'tags': ['메타데이터'],
+    'description': 'DMS에 관련된 링크 정보를 조회합니다.',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '정보 조회 성공',
+            'examples': {
+                '': {
+                    'Facebook': 'https://www.facebook.com/DMSforDSM/',
+                    'Github': 'https://github.com/DSM-DMS'
+                }
+            }
+        }
+    }
+}
