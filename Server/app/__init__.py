416326bb25182135ca669e22a3f9a96a39e74e67
JoMingyu <city7310@naver.com>
2018-04-23 20:20:04 +0900

    UPDATE: Swagger URI v2도 환경 변수를 참조하도록

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, Flask, render_template
+import os
+
+from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flasgger import Swagger
@@ -40,7 +42,7 @@ def merge_v2_api(app_):
     from app.views.v2 import Router
     from app.docs.v2 import TEMPLATE
 
-    app_.config['SWAGGER']['specs_route'] = '/v2/docs'
+    app_.config['SWAGGER']['specs_route'] = os.getenv('SWAGGER_URI_V2', '/v2/docs')
     app_.config['SWAGGER']['basePath'] = '/v2'
 
     Swagger(template=TEMPLATE).init_app(app_)
