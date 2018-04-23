f357f6d9940945706012ab9e00ff36e0fcc018f7
JoMingyu <city7310@naver.com>
2018-04-23 17:31:46 +0900

    DONE: Meal API 구조

---
+++ Server/app/views/v2/mixed/school_data/meal.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, Response, abort, g, request
+from flask_restful import Api
+from flasgger import swag_from
+
+from app.docs.v2.mixed.school_data.meal import *
+from app.views.v2 import BaseResource, auth_required, json_required
+
+api = Api(Blueprint(__name__, __name__, url_prefix='/meal'))
+
+
+@api.resource('/')
+class Meal(BaseResource):
+    @swag_from(MEAL_GET)
+    def get(self):
+        pass
