71d437ccf9b7c7f3b71a401a79594cf8eb131cd6
JoMingyu <city7310@naver.com>
2018-04-25 00:05:32 +0900

    UPDATE: point data inserter start 구문 제거

--- Server/server.py
+++ Server/server.py
@@ -26,7 +26,4 @@ if __name__ == '__main__':
     from utils import extension_apply_cleaner
     Process(target=extension_apply_cleaner.run).start()
 
-    from utils.temp_scripts.point_data_inserter import do
-    Process(target=do).start()
-
     app.run(host=app.config['HOST'], port=int(args.port) if args.port else app.config['PORT'], debug=app.debug, threaded=True)
