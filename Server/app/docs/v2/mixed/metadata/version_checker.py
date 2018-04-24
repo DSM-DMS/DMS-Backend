e99007438d468de9c9a239f688495174ede5f4ad
JoMingyu <city7310@naver.com>
2018-04-24 19:19:01 +0900

    ADD: version check api swagger doc

---
+++ Server/app/docs/v2/mixed/metadata/version_checker.py
@@ -0,0 +1,33 @@
+VERSION_CHECK_GET = {
+    'tags': ['[Mixed] 메타데이터'],
+    'description': '해당 플랫폼의 버전이 최신인지 조회합니다.',
+    'parameters': [
+        {
+            'name': 'platform',
+            'description': '플랫폼 number(1: 웹, 2: 안드로이드, 3: iOS)',
+            'in': 'query',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'version',
+            'description': '해당 클라이언트의 버전',
+            'in': 'query',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '최신 버전임',
+        },
+        '204': {
+            'description': '최신 버전이 아니며, 해당 플랫폼의 최신 버전을 응답합니다.',
+            'examples': {
+                '': {
+                    'version': '1.9'
+                }
+            }
+        }
+    }
+}
