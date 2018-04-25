34a990d33eb40d17d8c79d8acf6960b91aecf500
JoMingyu <city7310@naver.com>
2018-04-25 20:12:19 +0900

    DONE: TCBase

---
+++ Server/tests_v2/views/__init__.py
@@ -0,0 +1,107 @@
+import ujson
+from binascii import hexlify
+from hashlib import pbkdf2_hmac
+from unittest import TestCase as TC
+
+from flask import Response
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+from app import app
+from app.models.account import AdminModel, StudentModel
+
+
+class TCBase(TC):
+    def __init__(self, *args, **kwargs):
+        self.client = app.test_client()
+
+        super(TCBase, self).__init__(*args, **kwargs)
+
+    def _create_fake_account(self):
+        AdminModel(
+            id=self.admin_id,
+            pw=self.encrypted_pw,
+            name=self.admin_name
+        ).save()
+
+        StudentModel(
+            id=self.student_id,
+            pw=self.encrypted_pw,
+            name=self.student_name,
+            number=self.student_number
+        ).save()
+
+    def _get_tokens(self):
+        with app.app_context():
+            self.admin_access_token = 'JWT {}'.format(create_access_token(self.admin_id))
+            self.admin_refresh_token = 'JWT {}'.format(create_refresh_token(self.admin_id))
+            self.student_access_token = 'JWT {}'.format(create_access_token(self.student_id))
+            self.student_refresh_token = 'JWT {}'.format(create_refresh_token(self.student_id))
+
+    def setUp(self):
+        self.admin_id = self.admin_name = 'admin'
+        self.student_id = self.student_name = 'student'
+        self.student_number = 3118
+        self.pw = 'pw'
+        self.encrypted_pw = hexlify(
+            pbkdf2_hmac(
+                'sha256',
+                self.pw.encode(),
+                app.secret_key.encode(),
+                100000
+            )
+        ).decode()
+
+        self._create_fake_account()
+        self._get_tokens()
+
+    def tearDown(self):
+        AdminModel.objects.delete()
+        StudentModel.objects.delete()
+
+    def json_request(self, method, target_url_rule, token, *args, **kwargs):
+        """
+        Helper for json request
+
+        Args:
+            method (func): Request method
+            target_url_rule (str): URL rule for request
+            token (str) : JWT or OAuth's access token with prefix(Bearer, JWT, ...)
+
+        Returns:
+            Response
+        """
+        data = kwargs.pop('data')
+
+        return method(
+            target_url_rule,
+            data=ujson.dumps(data) if data else None,
+            content_type='application/json',
+            headers={'Authorization': token or self.admin_access_token},
+            *args,
+            **kwargs
+        )
+
+    def request(self, method, target_url_rule, token=None, *args, **kwargs):
+        """
+        Helper for common request
+
+        Args:
+            method (func): Request method
+            target_url_rule (str): URL rule for request
+            token (str) : JWT or OAuth's access token with prefix(Bearer, JWT, ...)
+
+        Returns:
+            Response
+        """
+        return method(
+            target_url_rule,
+            headers={'Authorization': token or self.admin_access_token},
+            *args,
+            **kwargs
+        )
+
+    def decode_response_data(self, resp):
+        return resp.data.decode()
+
+    def get_response_data(self, resp):
+        return ujson.loads(self.decode_response_data(resp))
