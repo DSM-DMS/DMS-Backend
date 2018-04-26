a89480e269058734925db475492351c7f2d3661f
RISMME <python@istruly.sexy>
2018-04-26 15:46:46 +0900

    DONE: Stunet test frame

---
+++ Server/tests_v2/views/student/apply/extension.py
@@ -0,0 +1,21 @@
+from tests_v2.views import TCBase
+
+
+class TestStudentApplyExtension(TCBase):
+    """
+    This TC tests
+        * METHOD /
+    """
+    def setUp(self):
+        super(TestStudentApplyExtension, self).setUp()
+
+        # ---
+
+    def tearDown(self):
+        # ---
+
+        super(TestStudentApplyExtension, self).tearDown()
+
+    def test(self):
+        # -- Test --
+        pass
---
+++ Server/tests_v2/views/student/apply/goingout.py
@@ -0,0 +1,21 @@
+from tests_v2.views import TCBase
+
+
+class TestStudentApplyGoingout(TCBase):
+    """
+    This TC tests
+        * METHOD /
+    """
+    def setUp(self):
+        super(TestStudentApplyGoingout, self).setUp()
+
+        # ---
+
+    def tearDown(self):
+        # ---
+
+        super(TestStudentApplyGoingout, self).tearDown()
+
+    def test(self):
+        # -- Test --
+        pass
