391c7f628ed32f763dd1ec164cfb364e5e4d3ade
JoMingyu <city7310@naver.com>
2018-04-26 20:13:17 +0900

    UPDATE: run_v2_test()의 모듈 네이밍 패턴 수정

--- Server/run_test.py
+++ Server/run_test.py
@@ -8,13 +8,13 @@ def run_v1_test():
     ut.TextTestRunner().run(all_tests)
 
 
-# def run_v2_test():
-#     all_tests = ut.TestLoader().discover('tests_v2', 'test_*.py')
-#     ut.TextTestRunner().run(all_tests)
+def run_v2_test():
+    all_tests = ut.TestLoader().discover('tests_v2', 't_*.py')
+    ut.TextTestRunner().run(all_tests)
 
 
 if __name__ == '__main__':
     app.testing = True
 
     run_v1_test()
-    # run_v2_test()
+    run_v2_test()
