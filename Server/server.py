869f402c64573d5985a6dc9bd10d792206ef73d0
JoMingyu <city7310@naver.com>
2018-04-26 19:31:06 +0900

    UPDATE: Argument parser 개선

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process
+import os
 
 from app import app
 
@@ -7,10 +8,9 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port')
+    parser.add_argument('-p', '--port', type=int)
     args = parser.parse_args()
 
-    import os
     if 'SECRET_KEY' not in os.environ:
         print('[WARN] SECRET KEY is not set in the environment variable !!')
 
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     from utils import extension_apply_cleaner
     Process(target=extension_apply_cleaner.run).start()
 
-    app.run(host=app.config['HOST'], port=int(args.port) if args.port else app.config['PORT'], debug=app.debug, threaded=True)
+    app.run(host=app.config['HOST'], port=args.port or app.config['PORT'], debug=app.debug, threaded=True)
