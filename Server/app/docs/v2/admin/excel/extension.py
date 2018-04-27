08fb33d6f02f769ba893bd36150a49b9f23650fa
JoMingyu <city7310@naver.com>
2018-04-27 16:09:38 +0900

    UPDATE: 연장신청 엑셀 다운로드 API를 11시/12시로 나눔

--- Server/app/docs/v2/admin/excel/extension.py
+++ Server/app/docs/v2/admin/excel/extension.py
@@ -1,3 +1,4 @@
 from app.docs.v2.admin.excel import generate_excel_doc
 
-EXTENSION_EXCEL_DOWNLOAD_GET = generate_excel_doc('연장')
+EXTENSION_11_EXCEL_DOWNLOAD_GET = generate_excel_doc('11시 연장')
+EXTENSION_12_EXCEL_DOWNLOAD_GET = generate_excel_doc('12시 연장')
--- Server/app/views/v2/admin/excel/extension.py
+++ Server/app/views/v2/admin/excel/extension.py
@@ -5,11 +5,18 @@ from flasgger import swag_from
 from app.docs.v2.admin.excel.extension import *
 from app.views.v2 import BaseResource, auth_required, json_required
 
-api = Api(Blueprint(__name__, __name__, url_prefix='/admin'))
+api = Api(Blueprint(__name__, __name__, url_prefix='/admin/extension'))
 
 
-@api.resource('/extension')
-class ExtensionExcelDownload(BaseResource):
-    @swag_from(EXTENSION_EXCEL_DOWNLOAD_GET)
+@api.resource('/11')
+class Extension11ExcelDownload(BaseResource):
+    @swag_from(EXTENSION_11_EXCEL_DOWNLOAD_GET)
+    def get(self):
+        pass
+
+
+@api.resource('/12')
+class Extension12ExcelDownload(BaseResource):
+    @swag_from(EXTENSION_12_EXCEL_DOWNLOAD_GET)
     def get(self):
         pass
