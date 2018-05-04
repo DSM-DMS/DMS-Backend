a1526f529a87d0ec0d60c71d64f910c0b597ca6a
JoMingyu <city7310@naver.com>
2018-05-04 13:54:14 +0900

    UPDATE: 중복을 제거하기 위한 상속 구조 활용

---
+++ Server/app/views/v2/admin/excel/__init__.py
@@ -0,0 +1,35 @@
+from openpyxl import Workbook
+
+from flask import make_response, send_from_directory
+
+from app.views.v2 import BaseResource
+
+from utils.excel_style_manager import ready_applyment_worksheet
+
+
+class ExcelDownload(BaseResource):
+    def __init__(self, model, filename, extension='xlsx'):
+        self.model = model
+        self.filename = filename
+        self.extension = extension
+        self.filename_full = '{}.{}'.format(self.filename, self.extension)
+
+        super(ExcelDownload, self).__init__()
+
+    def ready_worksheet(self):
+        wb = Workbook()
+        ws = wb.active
+
+        ready_applyment_worksheet(ws)
+
+        return wb, ws
+
+    def save_excel(self, wb):
+        wb.save('{}'.format(self.filename_full))
+        wb.close()
+
+    def get_response_object_with_excel_file(self):
+        resp = make_response(send_from_directory('../', self.filename_full))
+        resp.headers.extend({'Cache-Control': 'no-cache'})
+
+        return resp
--- Server/app/views/v2/admin/excel/extension.py
+++ Server/app/views/v2/admin/excel/extension.py
@@ -1,40 +1,23 @@
-from openpyxl import Workbook
-
-from flask import Blueprint, make_response, send_from_directory
+from flask import Blueprint
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.excel.extension import *
 from app.models.account import AdminModel
 from app.models.apply import ExtensionApply11Model, ExtensionApply12Model
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.views.v2 import auth_required
+from app.views.v2.admin.excel import ExcelDownload
 
-from utils.excel_style_manager import get_cell_positions_from_student_number, ready_applyment_worksheet
+from utils.excel_style_manager import get_cell_positions_from_student_number
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin/extension'))
 
 
-class ExtensionExcelDownload(BaseResource):
-    def __init__(self, model, filename, extension='.xlsx'):
+class ExtensionExcelDownload(ExcelDownload):
+    def __init__(self, *args, **kwargs):
         self.EXTENSION_CLASSES = '가온실', '나온실', '다온실', '라온실', '3층', '4층', '5층', '2층'
-        self.model = model
-        self.filename = filename
-        self.extension = extension
-        self.filename_full = '{}.{}'.format(self.filename, self.extension)
 
-        super(ExtensionExcelDownload, self).__init__()
-
-    def ready_worksheet(self):
-        wb = Workbook()
-        ws = wb.active
-
-        ready_applyment_worksheet(ws)
-
-        return wb, ws
-
-    def save_excel(self, wb):
-        wb.save('{}'.format(self.filename_full))
-        wb.close()
+        super(ExtensionExcelDownload, self).__init__(*args, **kwargs)
 
     def generate_excel(self):
         wb, ws = self.ready_worksheet()
@@ -50,18 +33,11 @@ class ExtensionExcelDownload(BaseResource):
 
         self.save_excel(wb)
 
-    def get_response_object_with_excel_file(self):
-        resp = make_response(send_from_directory('../', self.filename_full))
-        resp.headers.extend({'Cache-Control': 'no-cache'})
-
-        return resp
-
 
 @api.resource('/11')
 class Extension11ExcelDownload(ExtensionExcelDownload):
     def __init__(self):
         super(Extension11ExcelDownload, self).__init__(ExtensionApply11Model, '11')
-        # TODO filename에 문제 있어서 404 나는거임
 
     @auth_required(AdminModel)
     @swag_from(EXTENSION_11_EXCEL_DOWNLOAD_GET)
