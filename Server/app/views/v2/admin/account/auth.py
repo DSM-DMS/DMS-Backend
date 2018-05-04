409e963ef3efb0469c58d1ad77cbb1e44857571a
JoMingyu <city7310@naver.com>
2018-05-04 12:11:07 +0900

    DONE: POST /v2/admin/auth

--- Server/app/views/v2/admin/account/auth.py
+++ Server/app/views/v2/admin/account/auth.py
@@ -1,15 +1,51 @@
-from flask import Blueprint, Response, abort, g, request
+from binascii import hexlify
+from hashlib import pbkdf2_hmac
+from uuid import uuid4
+
+from flask import Blueprint, abort, current_app, request
+from flask_jwt_extended import create_access_token, create_refresh_token
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.account.auth import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.models.account import AdminModel, RefreshTokenModel
+from app.views.v2 import BaseResource, json_required
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin'))
 
 
 @api.resource('/auth')
 class Auth(BaseResource):
+    @json_required('id', 'password')
     @swag_from(AUTH_POST)
     def post(self):
-        pass
+        """
+        관리자 로그인
+        """
+        id = request.json['id']
+        password = request.json['password']
+
+        encrypted_password = hexlify(pbkdf2_hmac(
+            hash_name='sha256',
+            password=password.encode(),
+            salt=current_app.secret_key.encode(),
+            iterations=100000
+        )).decode('utf-8')
+
+        admin = AdminModel.objects(id=id, pw=encrypted_password).first()
+
+        if not admin:
+            abort(401)
+        else:
+            refresh_token = uuid4()
+
+            RefreshTokenModel(
+                token=refresh_token,
+                token_owner=admin,
+                pw_snapshot=encrypted_password
+            ).save()
+
+            return {
+                'accessToken': create_access_token(id),
+                'refreshToken': create_refresh_token(str(refresh_token))
+            }, 201
