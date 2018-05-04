164df67d80cdceac344e89acc934b0283455d485
JoMingyu <city7310@naver.com>
2018-05-04 13:58:39 +0900

    DONE: GET /admin/goingout

--- Server/app/views/v2/admin/excel/goingout.py
+++ Server/app/views/v2/admin/excel/goingout.py
@@ -1,15 +1,67 @@
-from flask import Blueprint, Response, abort, g, request
+from flask import Blueprint
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.excel.goingout import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.models.account import AdminModel
+from app.models.apply import GoingoutApplyModel, StayApplyModel
+from app.views.v2 import auth_required
+from app.views.v2.admin.excel import ExcelDownload
+
+from utils.excel_style_manager import get_cell_positions_from_student_number
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin'))
 
 
 @api.resource('/goingout')
-class GoingoutExcelDownload(BaseResource):
+class GoingoutExcelDownload(ExcelDownload):
+    def __init__(self):
+        self.model = GoingoutApplyModel
+        self.filename = 'goingout'
+
+        super(GoingoutExcelDownload, self).__init__(self.model, self.filename)
+
+    def generate_excel(self):
+        wb, ws = self.ready_worksheet()
+
+        for apply in GoingoutApplyModel.objects:
+            student = apply.student
+
+            number_cell, name_cell, status_cell = get_cell_positions_from_student_number(student)
+
+            stay_apply = StayApplyModel.objects(student=student).first()
+
+            if stay_apply.value < 3:
+                ws[number_cell] = None
+                ws[name_cell] = None
+                continue
+            else:
+                ws[number_cell] = student.number
+                ws[name_cell] = student.name
+
+            if apply.on_saturday and apply.on_sunday:
+                status = '토요일, 일요일 외출'
+            elif apply.on_saturday:
+                status = '토요일 외출'
+            elif apply.on_sunday:
+                status = '일요일 외출'
+            else:
+                status = ''
+            ws.column_dimensions['D'].width = 20
+            ws.column_dimensions['H'].width = 20
+            ws.column_dimensions['L'].width = 20
+            ws.column_dimensions['P'].width = 20
+
+            ws[status_cell] = status
+
+        self.save_excel(wb)
+
+    @auth_required(AdminModel)
     @swag_from(GOINGOUT_EXCEL_DOWNLOAD_GET)
     def get(self):
-        pass
+        """
+        외출신청 정보 엑셀 다운로드
+        """
+        self.generate_excel()
+
+        return self.get_response_object_with_excel_file()
