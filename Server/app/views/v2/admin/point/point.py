efe4065804b68b52c08ba36fd775c64f071d85bc
JoMingyu <city7310@naver.com>
2018-05-09 10:46:00 +0900

    DONE: GET /admin/point

--- Server/app/views/v2/admin/point/point.py
+++ Server/app/views/v2/admin/point/point.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.point.point import *
+from app.models.account import AdminModel, StudentModel
+from app.models.point import PointRuleModel, PointHistoryModel
 from app.views.v2 import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin'))
@@ -10,9 +12,26 @@ api = Api(Blueprint(__name__, __name__, url_prefix='/admin'))
 
 @api.resource('/point')
 class Point(BaseResource):
+    @auth_required(AdminModel)
     @swag_from(POINT_GET)
     def get(self):
-        pass
+        """
+        특정 학생의 상벌점 내역 조회
+        """
+        id = request.args['id']
+
+        student = StudentModel.objects(id=id).first()
+
+        if not student:
+            return Response('', 204)
+
+        return self.unicode_safe_json_dumps([{
+            'id': str(history.id),
+            'date': history.time.strftime('%Y-%m-%d'),
+            'reason': history.reason,
+            'pointType': history.point_type,
+            'point': history.point
+        } for history in student.point_histories])
 
     @swag_from(POINT_POST)
     def post(self):
