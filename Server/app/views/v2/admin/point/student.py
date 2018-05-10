5794055f8a35001f7d19f0e747cae1e8026d16de
JoMingyu <city7310@naver.com>
2018-05-10 13:21:52 +0900

    DONE: GET /admin/post/student

--- Server/app/views/v2/admin/point/student.py
+++ Server/app/views/v2/admin/point/student.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.point.student import *
+from app.models.account import AdminModel, StudentModel
 from app.views.v2 import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin/point/student'))
@@ -10,9 +11,27 @@ api = Api(Blueprint(__name__, __name__, url_prefix='/admin/point/student'))
 
 @api.resource('')
 class StudentList(BaseResource):
+    @auth_required(AdminModel)
     @swag_from(STUDENT_LIST_GET)
     def get(self):
-        pass
+        """
+        학생 리스트 조회
+        """
+        return self.unicode_safe_json_dumps([{
+            'id': student.id,
+            'name': student.name,
+            'number': student.number,
+            'goodPoint': student.good_point,
+            'badPoint': student.bad_point,
+            'penaltyLevel': student.penalty_level,
+            'pointHistories': [{
+                'time': history.time.strftime('%Y-%m-%d %H:%M:%S'),
+                'reason': history.reason,
+                'pointType': history.point_type,
+                'point': history.point
+            } for history in student.point_histories],
+            'penaltyTrainingStatus': student.penalty_training_status
+        } for student in StudentModel.objects])
 
 
 @api.resource('/penalty')
