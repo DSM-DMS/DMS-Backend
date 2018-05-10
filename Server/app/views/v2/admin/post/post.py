dbb8758a9663ba83c776c4e8a91ce74a70a5f24a
JoMingyu <city7310@naver.com>
2018-05-10 20:51:38 +0900

    UPDATE: import for debug

--- Server/app/views/v2/admin/post/post.py
+++ Server/app/views/v2/admin/post/post.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.post.post import *
+from app.models.account import AdminModel
+from app.models.post import FAQModel, NoticeModel, RuleModel
 from app.views.v2 import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin/post'))
