b5027000b852b5891cd28b798609f23e14dd2bcb
JoMingyu <city7310@naver.com>
2018-05-10 11:09:49 +0900

    ADD: webhook.py

---
+++ Server/webhook.py
@@ -0,0 +1,22 @@
+import socket
+
+import json
+from flask import Flask, request
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9999
+
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+
+    @app.route('/')
+    def event_handler():
+        payload = request.json
+        print(json.dumps(payload, indent=4))
+        print('Working!')
+
+        return 'hello'
+
+
+    app.run(host=HOST, port=PORT, threaded=True)
