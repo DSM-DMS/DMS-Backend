98cdce79609adad13caf7265a11955359b2da596
JoMingyu <city7310@naver.com>
2018-05-11 22:33:58 +0900

    DONE: GET /admin/report/facility API

--- Server/app/views/v2/admin/report/facility.py
+++ Server/app/views/v2/admin/report/facility.py
@@ -3,7 +3,9 @@ from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.report.facility import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.models.account import AdminModel
+from app.models.report import FacilityReportModel
+from app.views.v2 import BaseResource, auth_required
 
 api = Api(Blueprint(__name__, __name__))
 api.prefix = '/admin/report'
@@ -11,11 +13,18 @@ api.prefix = '/admin/report'
 
 @api.resource('/facility')
 class FacilityReport(BaseResource):
+    @auth_required(AdminModel)
     @swag_from(FACILITY_REPORT_GET)
     def get(self):
         """
         시설고장신고 조회
         """
+        return [{
+            'id': str(report.id),
+            'author': report.author,
+            'content': report.content,
+            'room': report.room
+        } for report in FacilityReportModel.objects]
 
 
 @api.resource('/facility/<id>')
