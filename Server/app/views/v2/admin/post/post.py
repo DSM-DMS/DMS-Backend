87afd806f4124698caca7c83ae094988fdc8d408
JoMingyu <city7310@naver.com>
2018-05-11 10:40:39 +0900

    DONE: POST /admin/post/<category>

--- Server/app/views/v2/admin/post/post.py
+++ Server/app/views/v2/admin/post/post.py
@@ -9,12 +9,36 @@ from app.views.v2 import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/admin/post'))
 
+CATEGORY_MODEL_MAPPING = {
+    'FAQ': FAQModel,
+    'NOTICE': NoticeModel,
+    'RULE': RuleModel
+}
+
 
 @api.resource('/<category>')
 class Post(BaseResource):
+    @auth_required(AdminModel)
     @swag_from(POST_POST)
     def post(self, category):
-        pass
+        """
+        게시글 업로드
+        """
+        title = request.json['title']
+        content = request.json['content']
+
+        if category.upper() not in CATEGORY_MODEL_MAPPING:
+            abort(400)
+
+        post = CATEGORY_MODEL_MAPPING[category.upper()](
+            author=g.user.name,
+            title=title,
+            content=content
+        ).save()
+
+        return {
+            'id': str(post.id)
+        }, 201
 
 
 @api.resource('/<category>/<post_id>')
