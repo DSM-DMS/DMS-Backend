40b362df4301110f0cdbdf58a28a99e16e806eae
JoMingyu <city7310@naver.com>
2018-05-11 11:18:57 +0900

    DONE: PATCH /admin/post-preview/<category>

--- Server/app/views/v2/admin/post/preview.py
+++ Server/app/views/v2/admin/post/preview.py
@@ -3,7 +3,9 @@ from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.admin.post.preview import *
+from app.models.account import AdminModel
 from app.views.v2 import BaseResource, auth_required, json_required
+from app.views.v2.admin.post import CATEGORY_MODEL_MAPPING
 
 api = Api(Blueprint(__name__, __name__))
 api.prefix = '/admin/post-preview'
@@ -11,8 +13,34 @@ api.prefix = '/admin/post-preview'
 
 @api.resource('/<category>')
 class Preview(BaseResource):
+    @auth_required(AdminModel)
+    @json_required('id')
     @swag_from(PREVIEW_PATCH)
     def patch(self, category):
         """
         게시글 프리뷰 설정
         """
+        id = request.json['id']
+
+        if category.upper() not in CATEGORY_MODEL_MAPPING:
+            abort(400)
+
+        if len(id) != 24:
+            return Response('', 204)
+
+        post = CATEGORY_MODEL_MAPPING[category.upper()].objects(id=id).first()
+
+        if not post:
+            return Response('', 204)
+
+        pinned_post = CATEGORY_MODEL_MAPPING[category.upper()].objects(pinned=True).first()
+
+        if pinned_post:
+            pinned_post.update(pinned=False)
+
+        updated = post.update(pinned=True)
+
+        if updated:
+            return Response('', 200)
+        else:
+            abort(500)
