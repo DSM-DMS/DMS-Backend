b7686f2bc30e4efd376265add0f6c3509b3042e3
JoMingyu <city7310@naver.com>
2018-05-14 13:42:52 +0900

    DONE: GET /jwt/check API

--- Server/app/views/v2/mixed/jwt/checker.py
+++ Server/app/views/v2/mixed/jwt/checker.py
@@ -1,15 +1,31 @@
 from flask import Blueprint, Response, abort, g, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.mixed.jwt.checker import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.models.account import AdminModel, StudentModel, SystemModel
+from app.views.v2 import BaseResource
 
-api = Api(Blueprint(__name__, __name__, url_prefix='/jwt'))
+api = Api(Blueprint(__name__, __name__))
+api.prefix = '/jwt'
 
 
 @api.resource('/check')
 class AuthCheck(BaseResource):
+    @jwt_required
     @swag_from(AUTH_CHECK_GET)
     def get(self):
-        pass
+        """
+        로그인 여부 체크
+        """
+        if not any(
+            (
+                AdminModel.objects(id=get_jwt_identity()),
+                StudentModel.objects(id=get_jwt_identity()),
+                SystemModel.objects(id=get_jwt_identity())
+            )
+        ):
+            return Response('', 204)
+
+        return Response('', 200)
