85f3a0e27726bf6a011d088a0d15928ac97dbf0b
JoMingyu <city7310@naver.com>
2018-05-15 13:00:04 +0900

    DONE: /metadata/developer-info API

--- Server/app/docs/v2/mixed/metadata/developers.py
+++ Server/app/docs/v2/mixed/metadata/developers.py
@@ -1,31 +1,16 @@
 DEVELOPER_INFO_GET = {
     'tags': ['[Mixed] 메타데이터'],
     'description': 'DMS 개발자 정보를 조회합니다.',
-    'parameters': [
-        {
-            'name': 'Authorization',
-            'description': 'JWT Token(JWT ***)',
-            'in': 'header',
-            'type': 'str',
-            'required': True
-        }
-    ],
     'responses': {
         '200': {
             'description': '정보 조회 성공',
             'examples': {
                 '': {
-                    'App': [
-                        '조성빈',
-                        '이병찬',
-                        '윤정현',
-                        '이성현'
-                    ],
-                    'Backend': [
-                        '김성래',
-                        '조민규',
-                        '인상민'
-                    ]
+                    'app': ['조성빈', '이병찬', '윤정현', '이성현'],
+                    'server': ['김성래', '조민규', '인상민'],
+                    'webFrontend': ['김지수', '김건', '서윤호', '김형규', '오인서', '윤효상'],
+                    'desktop': ['김경식', '정원태', '김동현', '이종현', '류근찬'],
+                    'design': ['윤여환', '김동규']
                 }
             }
         }
--- Server/app/views/v2/mixed/metadata/developers.py
+++ Server/app/views/v2/mixed/metadata/developers.py
@@ -1,15 +1,25 @@
-from flask import Blueprint, Response, abort, g, request
+from flask import Blueprint
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.mixed.metadata.developers import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.views.v2 import BaseResource
 
-api = Api(Blueprint(__name__, __name__, url_prefix='/metadata'))
+api = Api(Blueprint(__name__, __name__))
+api.prefix = '/metadata'
 
 
 @api.resource('/developer-info')
 class DeveloperInfo(BaseResource):
     @swag_from(DEVELOPER_INFO_GET)
     def get(self):
-        pass
+        """
+        개발자 정보 조회
+        """
+        return self.unicode_safe_json_dumps({
+            'app': ['조성빈', '이병찬', '윤정현', '이성현'],
+            'server': ['김성래', '조민규', '인상민'],
+            'webFrontend': ['김지수', '김건', '서윤호', '김형규', '오인서', '윤효상'],
+            'desktop': ['김경식', '정원태', '김동현', '이종현', '류근찬'],
+            'design': ['윤여환', '김동규']
+        })
