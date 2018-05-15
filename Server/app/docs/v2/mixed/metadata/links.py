386de88e67700f0969eb7d28bd2bd69f960864bb
JoMingyu <city7310@naver.com>
2018-05-15 13:31:10 +0900

    DONE: GET /metadata/links API

--- Server/app/docs/v2/mixed/metadata/links.py
+++ Server/app/docs/v2/mixed/metadata/links.py
@@ -1,22 +1,15 @@
 LINKS_GET = {
     'tags': ['[Mixed] 메타데이터'],
     'description': 'DMS에 관련된 링크 정보를 조회합니다.',
-    'parameters': [
-        {
-            'name': 'Authorization',
-            'description': 'JWT Token(JWT ***)',
-            'in': 'header',
-            'type': 'str',
-            'required': True
-        }
-    ],
     'responses': {
         '200': {
             'description': '정보 조회 성공',
             'examples': {
                 '': {
-                    'Facebook': 'https://www.facebook.com/DMSforDSM/',
-                    'Github': 'https://github.com/DSM-DMS'
+                    'facebook': 'https://www.facebook.com/DMSforDSM/',
+                    'github': 'https://github.com/DSM-DMS',
+                    'android': 'https://play.google.com/store/apps/details?id=teamdms.dms_kotlin',
+                    'ios': 'https://itunes.apple.com/KR/app/id1328234395?mt=8'
                 }
             }
         }
--- Server/app/views/v2/mixed/metadata/links.py
+++ Server/app/views/v2/mixed/metadata/links.py
@@ -1,15 +1,24 @@
-from flask import Blueprint, Response, abort, g, request
+from flask import Blueprint
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.mixed.metadata.links import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.views.v2 import BaseResource
 
-api = Api(Blueprint(__name__, __name__, url_prefix='/metadata'))
+api = Api(Blueprint(__name__, __name__))
+api.prefix = '/metadata'
 
 
 @api.resource('/links')
 class Links(BaseResource):
     @swag_from(LINKS_GET)
     def get(self):
-        pass
+        """
+        DMS 관련 링크 조회
+        """
+        return {
+            'facebook': 'https://www.facebook.com/DMSforDSM/',
+            'github': 'https://github.com/DSM-DMS',
+            'android': 'https://play.google.com/store/apps/details?id=teamdms.dms_kotlin',
+            'ios': 'https://itunes.apple.com/KR/app/id1328234395?mt=8'
+        }
