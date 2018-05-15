be99165d1c460d7e8d4348f7bfbc29230722624f
JoMingyu <city7310@naver.com>
2018-05-15 14:13:26 +0900

    DONE: GET /metadata/developer-info TC

---
+++ Server/tests/v2/views/mixed/metadata/t_developers.py
@@ -0,0 +1,41 @@
+from tests.v2.views import TCBase
+
+
+class TestDeveloperLoad(TCBase):
+    """
+    개발자 목록 조회를 테스트합니다.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TestDeveloperLoad, self).__init__(*args, **kwargs)
+
+        self.method = self.client.get
+        self.target_uri = '/metadata/developer-info'
+
+    def setUp(self):
+        super(TestDeveloperLoad, self).setUp()
+
+        # ---
+
+        self.expected_response_data = {
+            'app': ['조성빈', '이병찬', '윤정현', '이성현'],
+            'server': ['김성래', '조민규', '인상민'],
+            'webFrontend': ['김지수', '김건', '서윤호', '김형규', '오인서', '윤효상'],
+            'desktop': ['김경식', '정원태', '김동현', '이종현', '류근찬'],
+            'design': ['윤여환', '김동규']
+        }
+
+        self._request = lambda: self.request(
+            self.method,
+            self.target_uri
+        )
+
+    def testLoadSuccess(self):
+        # (1) 개발자 정보 조회
+        resp = self._request()
+
+        # (2) status code 200
+        self.assertEqual(resp.status_code, 200)
+
+        # (3) response data
+        self.assertDictEqual(resp.json, self.expected_response_data)
