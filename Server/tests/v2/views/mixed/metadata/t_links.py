086b5dc29ed15038b8ada705beabcaea1414d83e
JoMingyu <city7310@naver.com>
2018-05-15 14:13:33 +0900

    DONE: GET /metadata/links TC

---
+++ Server/tests/v2/views/mixed/metadata/t_links.py
@@ -0,0 +1,40 @@
+from tests.v2.views import TCBase
+
+
+class TestDeveloperLoad(TCBase):
+    """
+    개발자 목록 조회를 테스트합니다.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TestDeveloperLoad, self).__init__(*args, **kwargs)
+
+        self.method = self.client.get
+        self.target_uri = '/metadata/links'
+
+    def setUp(self):
+        super(TestDeveloperLoad, self).setUp()
+
+        # ---
+
+        self.expected_response_data = {
+            'facebook': 'https://www.facebook.com/DMSforDSM/',
+            'github': 'https://github.com/DSM-DMS',
+            'android': 'https://play.google.com/store/apps/details?id=teamdms.dms_kotlin',
+            'ios': 'https://itunes.apple.com/KR/app/id1328234395?mt=8'
+        }
+
+        self._request = lambda: self.request(
+            self.method,
+            self.target_uri
+        )
+
+    def testLoadSuccess(self):
+        # (1) 개발자 정보 조회
+        resp = self._request()
+
+        # (2) status code 200
+        self.assertEqual(resp.status_code, 200)
+
+        # (3) response data
+        self.assertDictEqual(resp.json, self.expected_response_data)
