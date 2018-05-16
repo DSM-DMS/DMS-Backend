6de189eb0ecf802bfe18fc90a3a2d089c94f6004
JoMingyu <city7310@naver.com>
2018-05-16 12:32:05 +0900

    Revert "TEMP: InfluxDB에 404가 제대로 적재되는지 테스트하기 위해 /admin 라우팅 임시 제거"
    This reverts commit af0167551167a777869a0f1d28e81364d1e15b84.

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -50,7 +50,7 @@ def create_app(*config_cls):
     app_.after_request(after_request)
     app_.register_error_handler(Exception, exception_handler)
     app_.add_url_rule('/', view_func=index_student)
-    # app_.add_url_rule('/admin', view_func=index_admin)
+    app_.add_url_rule('/admin', view_func=index_admin)
     app_.add_url_rule('/hook', view_func=webhook_event_handler, methods=['POST'])
 
     return app_
