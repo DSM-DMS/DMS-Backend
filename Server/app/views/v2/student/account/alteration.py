c297d2c06f87366d4721f385a454ea2d952611e9
RISMME <python@istruly.sexy>
2018-05-28 23:45:38 +0900

    DONE: Student password change API

--- Server/app/views/v2/student/account/alteration.py
+++ Server/app/views/v2/student/account/alteration.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, Response, abort, g, request
+from binascii import hexlify
+from hashlib import pbkdf2_hmac
+
+from flask import Blueprint, Response, abort, g, request, current_app
+from flask_jwt_extended import get_jwt_identity
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.student.account.alteration import *
+from app.models.account import StudentModel
 from app.views.v2 import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__))
@@ -18,6 +23,19 @@ class Account(BaseResource):
 
 @api.resource('/change-pw')
 class ChangePW(BaseResource):
+    @json_required({'currentPassword': str, 'newPassword': str})
+    @auth_required(StudentModel)
     @swag_from(CHANGE_PW_POST)
     def post(self):
-        pass
+        student = StudentModel.objects(id=get_jwt_identity()).first()
+
+        current_password = request.json['currentPassword']
+        new_password = request.json['newPassword']
+
+        encrypted_current_password = self.encrypt(current_password)
+
+        if student.pw != encrypted_current_password:
+            abort(403)
+
+        student.update(pw=new_password)
+        return Response('', 200)
