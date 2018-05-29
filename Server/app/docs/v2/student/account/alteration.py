b16234d10972eb1b35cc1e5be6128abdda38fcd2
RISMME <python@istruly.sexy>
2018-05-29 23:09:29 +0900

    DELETE: Student account delete api

--- Server/app/docs/v2/student/account/alteration.py
+++ Server/app/docs/v2/student/account/alteration.py
@@ -33,39 +33,3 @@ CHANGE_PW_POST = {
         }
     }
 }
-
-ACCOUNT_DELETE = {
-    'tags': ['[Student] 계정 관리'],
-    'description': '학생 자신의 계정을 삭제합니다.',
-    'parameters': [
-        {
-            'name': 'Authorization',
-            'description': 'JWT Token',
-            'in': 'header',
-            'type': 'str',
-            'required': True
-        },
-        {
-            'name': 'id',
-            'description': '현재 아이디',
-            'in': 'json',
-            'type': 'str',
-            'required': True
-        },
-        {
-            'name': 'password',
-            'description': '현재 비밀번호',
-            'in': 'json',
-            'type': 'str',
-            'required': True
-        }
-    ],
-    'responses': {
-        '200': {
-            'description': '계정 삭제 성공'
-        },
-        '403': {
-            'description': '비밀번호 변경 실패(틀린 ID나 비밀번호), 또는 권한 없음'
-        }
-    }
-}
--- Server/app/views/v2/student/account/alteration.py
+++ Server/app/views/v2/student/account/alteration.py
@@ -1,7 +1,4 @@
-from binascii import hexlify
-from hashlib import pbkdf2_hmac
-
-from flask import Blueprint, Response, abort, g, request, current_app
+from flask import Blueprint, Response, abort, request
 from flask_jwt_extended import get_jwt_identity
 from flask_restful import Api
 from flasgger import swag_from
@@ -12,13 +9,6 @@ from app.views.v2 import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__))
 api.prefix = '/student/account'
-
-
-@api.resource('')
-class Account(BaseResource):
-    @swag_from(ACCOUNT_DELETE)
-    def delete(self):
-        pass
 
 
 @api.resource('/change-pw')
