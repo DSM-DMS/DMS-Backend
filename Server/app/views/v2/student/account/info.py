f06b7668c379cf5014d5fbac8af124ea831c0479
RISMME <python@istruly.sexy>
2018-05-30 18:36:46 +0900

    DONE: Check Apply status API

--- Server/app/views/v2/student/account/info.py
+++ Server/app/views/v2/student/account/info.py
@@ -3,7 +3,9 @@ from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.student.account.info import *
-from app.views.v2 import BaseResource, auth_required, json_required
+from app.models.account import StudentModel
+from app.models.apply import ExtensionApply11Model, ExtensionApply12Model, GoingoutApplyModel, StayApplyModel
+from app.views.v2 import BaseResource, auth_required
 
 api = Api(Blueprint(__name__, __name__))
 api.prefix = '/student/info'
@@ -12,8 +14,33 @@ api.prefix = '/student/info'
 @api.resource('/apply')
 class ApplyInfo(BaseResource):
     @swag_from(APPLY_INFO_GET)
+    @auth_required(StudentModel)
     def get(self):
-        pass
+        """
+        학생 신청 정보 확인
+        """
+        student = g.user
+        extension11 = ExtensionApply11Model(student=student).first()
+        extension12 = ExtensionApply12Model(student=student).first()
+
+        goingout = GoingoutApplyModel(student=student).first()
+        stay = StayApplyModel.objects(student=student).first()
+
+        return self.unicode_safe_json_dumps({
+            "extension11": {
+                "class": extension11.class_,
+                "seat": extension11.seat
+            } if extension11 else None,
+            "extension12": {
+                "class": extension12.class_,
+                "seat": extension12.seat
+            } if extension12 else None,
+            "goingout": {
+                "sat": goingout.on_saturday,
+                "sun": goingout.on_sunday
+            } if goingout else None,
+            "stay": stay.value if stay else None
+        })
 
 
 @api.resource('/mypage')
