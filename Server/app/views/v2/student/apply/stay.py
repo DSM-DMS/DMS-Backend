d14ed267c2e5adfec4563ac9a63ebdb8f120bc0b
JoMingyu <city7310@naver.com>
2018-06-07 22:02:03 +0900

    [UPDATE] [TEMP] 잔류신청 가능 시간을 오후 11시까지로 변경

--- Server/app/views/v2/student/apply/stay.py
+++ Server/app/views/v2/student/apply/stay.py
@@ -38,7 +38,7 @@ class Stay(BaseResource):
         """
         student = g.user
         now = datetime.now()
-        if current_app.testing or (now.weekday() == 6 and now.time() > time(20, 30)) or (0 <= now.weekday() < 3) or (now.weekday() == 3 and now.time() < time(22, 00)):
+        if current_app.testing or (now.weekday() == 6 and now.time() > time(20, 30)) or (0 <= now.weekday() < 3) or (now.weekday() == 3 and now.time() < time(23, 00)):
             # 신청 가능 범위
             # - 일요일 오후 8시 30분 이후부터 목요일 오후 10시까지
             value = request.json['value']
