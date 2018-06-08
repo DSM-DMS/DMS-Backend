25ec13b369d18510b40d37d7bbeb4531a97125df
JoMingyu <city7310@naver.com>
2018-06-08 23:08:57 +0900

    [UPDATE] 신청 정보 cleaner를 별도 스크립트로 작성
    - crontab으로 cronjob 돌릴 예정

---
+++ Server/apply_data_cleaner.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import time
+
+from mongoengine import *
+
+from config import Config
+connect(**Config.MONGODB_SETTINGS)
+
+from app.models.account import StudentModel
+from app.models.point import PointHistoryModel
+from app.models.apply import ExtensionApply11Model, ExtensionApply12Model, GoingoutApplyModel, StayApplyModel
+
+
+if 0 < datetime.now().time().hour < 12:
+    ExtensionApply11Model.objects.delete()
+    ExtensionApply12Model.objects.delete()
+    time.sleep(3600)
+
+
+def clear_duplicated_apply_data(student, model):
+    applies = model.objects(student=student)
+    if applies.count() >= 2:
+        print(student.number, student.name, '{} {}개'.format(model.__name__, goingout_applies.count()))
+
+        applies.order_by('apply_date')[0:applies.count() - 1].delete()
+
+
+for student in StudentModel.objects.order_by('number'):
+    clear_duplicated_apply_data(student, GoingoutApplyModel)
+    clear_duplicated_apply_data(student, StayApplyModel)
--- Server/server.py
+++ Server/server.py
@@ -13,9 +13,6 @@ if __name__ == '__main__':
     if 'SECRET_KEY' not in os.environ:
         print('[WARN] SECRET KEY is not set in the environment variable !!')
 
-    from utils import extension_apply_cleaner
-    Process(target=extension_apply_cleaner.run).start()
-
     from utils.meal_parser import parse
 
     now = datetime.now()
