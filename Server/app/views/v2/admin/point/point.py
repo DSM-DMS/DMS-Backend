dcd044f8a85506e4a40b0297fe4118d0486371ee
JoMingyu <city7310@naver.com>
2018-06-10 22:30:01 +0900

    [UPDATE] 상벌점 API 리팩토링

--- Server/app/views/v2/admin/point/point.py
+++ Server/app/views/v2/admin/point/point.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, abort, g, request
+from flask import Blueprint, Response, request
 from flask_restful import Api
 from flasgger import swag_from
 
@@ -21,16 +21,13 @@ class Point(BaseResource):
         """
         student = StudentModel.objects(id=student_id).first()
 
-        if not student:
-            return Response('', 204)
-
         return self.unicode_safe_json_dumps([{
             'id': str(history.id),
             'date': history.time.strftime('%Y-%m-%d'),
             'reason': history.reason,
             'pointType': history.point_type,
             'point': history.point
-        } for history in student.point_histories])
+        } for history in student.point_histories]) if student else Response('', 204)
 
     @auth_required(AdminModel)
     @json_required({'ruleId': str, 'applyGoodPoint': bool, 'point': int})
@@ -39,9 +36,11 @@ class Point(BaseResource):
         """
         특정 학생에게 상벌점 부여
         """
-        rule_id = request.json['ruleId']
-        apply_good_point = request.json['applyGoodPoint']
-        point = request.json['point']
+        payload = request.json
+
+        rule_id = payload['ruleId']
+        apply_good_point = payload['applyGoodPoint']
+        point = payload['point']
 
         student = StudentModel.objects(id=student_id).first()
 
@@ -83,7 +82,9 @@ class Point(BaseResource):
         """
         특정 학생의 상벌점 내역 삭제
         """
-        history_id = request.json['historyId']
+        payload = request.json
+
+        history_id = payload['historyId']
 
         student = StudentModel.objects(id=student_id).first()
 
@@ -99,9 +100,9 @@ class Point(BaseResource):
 
         student.point_histories = student.point_histories.exclude(id=history_id)
         if history.point_type:
-            student.update(good_point=student.good_point - history.point)
+            student.good_point = student.good_point - history.point
         else:
-            student.update(bad_point=student.bad_point - history.point)
+            student.bad_point = student.bad_point - history.point
 
         student.save()
 
