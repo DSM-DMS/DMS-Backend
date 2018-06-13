0610fa1a49ebf6b3a7b00ee84098f1ca528599d7
JoMingyu <city7310@naver.com>
2018-06-13 19:56:34 +0900

    [UPDATE] 상벌점 부여 API의 복잡성 제거

--- Server/app/views/v2/admin/point/point.py
+++ Server/app/views/v2/admin/point/point.py
@@ -36,6 +36,22 @@ class Point(BaseResource):
         """
         특정 학생에게 상벌점 부여
         """
+        def append_history(rule):
+            point_history = PointHistoryModel(
+                reason=rule.name,
+                point_type=rule.point_type,
+                point=point
+            )
+
+            student.point_histories.append(point_history)
+
+            return point_history.id
+
+        def update_penalty_level():
+            if (student.bad_point - 10) // 5 > student.penalty_level and not student.penalty_training_status:
+                student.penalty_level = (student.bad_point - 10) // 5
+                student.penalty_training_status = True
+
         payload = request.json
 
         apply_good_point = payload['applyGoodPoint']
@@ -50,29 +66,19 @@ class Point(BaseResource):
         if len(rule_id) != 24 or not PointRuleModel.objects(id=rule_id):
             return Response('', 205)
 
-        rule = PointRuleModel.objects(id=rule_id).first()
-
-        point_history = PointHistoryModel(
-            reason=rule.name,
-            point_type=rule.point_type,
-            point=point
-        )
-
-        student.point_histories.append(point_history)
+        history = append_history(PointRuleModel.objects(id=rule_id).first())
 
         if apply_good_point:
             student.good_point += point
         else:
             student.bad_point += point
 
-        if (student.bad_point - 10) // 5 > student.penalty_level and not student.penalty_training_status:
-            student.penalty_level = student.penalty_level + 1
-            student.penalty_training_status = True
+        update_penalty_level()
 
         student.save()
 
         return {
-            'id': str(point_history.id)
+            'id': str(history.id)
         }, 201
 
     @auth_required(AdminModel)
