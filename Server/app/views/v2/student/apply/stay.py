f8ec5a6f6993005cebc09147eb7b2a068414f0c6
JoMingyu <city7310@naver.com>
2018-06-13 19:42:39 +0900

    [UPDATE] POST&GET /v2/student/apply/stay API 리팩토링

--- Server/app/views/v2/student/apply/stay.py
+++ Server/app/views/v2/student/apply/stay.py
@@ -1,6 +1,6 @@
 from datetime import datetime, time
 
-from flask import Blueprint, Response, abort, g, request, current_app
+from flask import Blueprint, Response, g, request, current_app
 from flask_restful import Api
 from flasgger import swag_from
 
@@ -21,14 +21,8 @@ class Stay(BaseResource):
         """
         학생 잔류 신청 확인
         """
-        student = g.user
-
-        stay = StayApplyModel.objects(student=student).first()
-
-        return self.unicode_safe_json_dumps({
-            'value': stay.value or None
-        }) if stay else {
-            'value': 4
+        return {
+            'value': StayApplyModel.objects(student=g.user).first().value
         }
 
     @swag_from(STAY_POST)
@@ -38,15 +32,14 @@ class Stay(BaseResource):
         """
         학생 잔류 신청
         """
-        student = g.user
+        payload = request.json
+
         now = datetime.now()
+
         if current_app.testing or (now.weekday() == 6 and now.time() > time(20, 30)) or (0 <= now.weekday() < 3) or (now.weekday() == 3 and now.time() < time(23, 00)):
             # 신청 가능 범위
             # - 일요일 오후 8시 30분 이후부터 목요일 오후 10시까지
-            value = request.json['value']
-
-            StayApplyModel.objects(student=student).delete()
-            StayApplyModel(student=student, value=value).save()
+            StayApplyModel(student=g.user, value=payload['value']).save()
 
             return Response('', 201)
         else:
