b7c4d89452f2decf27f1908e746f11c7470b6394
JoMingyu <city7310@naver.com>
2018-06-14 09:56:20 +0900

    [DONE] goingout apply exporter

---
+++ Server/cron/mongo_to_influxdb_exporters/goingout_apply.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join('../..', 'app')))
+
+from mongoengine import *
+from influxdb import InfluxDBClient
+
+from models.apply import GoingoutApplyModel
+
+connect(**{
+    'db': 'dms-v2',
+    'host': None,
+    'port': None,
+    'username': os.getenv('MONGO_ID'),
+    'password': os.getenv('MONGO_PW')
+})
+
+CLIENT = InfluxDBClient(**{
+    'host': 'localhost',
+    'port': 8086,
+    'username': 'root',
+    'password': os.getenv('INFLUX_PW_{}'.format('DMS_V2'), 'root'),
+    'database': 'dms_v2'
+})
+MEASUREMENT = 'goingout_apply'
+
+saturday_goingout_applier = 0
+sunday_goingout_applier = 0
+
+for apply in GoingoutApplyModel.objects:
+    saturday_goingout_applier += 1 if apply.on_saturday else 0
+    sunday_goingout_applier += 1 if apply.on_sunday else 0
+
+    payload = [
+        {
+            'measurement': MEASUREMENT,
+            'fields': {
+                'saturday': saturday_goingout_applier,
+                'sunday': sunday_goingout_applier
+            }
+        }
+    ]
+
+    CLIENT.write(payload)
