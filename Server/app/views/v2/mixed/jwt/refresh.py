fa0190fa135eeeb4c0d3ab9aa25f57ec240061d9
JoMingyu <city7310@naver.com>
2018-07-11 15:56:54 +0900

    [UPDATE] v2 API의 token refresh 로직 변경

--- Server/app/views/v2/mixed/jwt/refresh.py
+++ Server/app/views/v2/mixed/jwt/refresh.py
@@ -1,12 +1,13 @@
 from uuid import UUID
 
 from flask import Blueprint, Response, abort, request
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_refresh_token_required
+from flask_jwt_extended import get_jwt_identity, jwt_refresh_token_required
 from flask_restful import Api
 from flasgger import swag_from
 
 from app.docs.v2.mixed.jwt.refresh import *
-from app.models.account import TokenModel, AccessTokenModel, RefreshTokenModel
+from app.models.account import RefreshTokenModel
+from app.models.token import AccessTokenModelV2, RefreshTokenModelV2
 from app.views.v2 import BaseResource
 
 api = Api(Blueprint(__name__, __name__))
@@ -22,11 +23,12 @@ class Refresh(BaseResource):
             token = RefreshTokenModel.objects(identity=UUID(get_jwt_identity())).first()
 
             if not token:
-                abort(401)
+                token = RefreshTokenModelV2.objects(identity=UUID(get_jwt_identity())).first()
+                if not token:
+                    abort(401)
 
             return {
-                'accessToken': create_access_token(
-                    TokenModel.generate_token(AccessTokenModel, token.owner, request.headers['USER-AGENT']))
+                'accessToken': AccessTokenModelV2.create_access_token(token.owner, request.headers['USER-AGENT'])
             } if token.owner.pw == token.pw_snapshot else Response('', 205)
         except ValueError:
             abort(422)
