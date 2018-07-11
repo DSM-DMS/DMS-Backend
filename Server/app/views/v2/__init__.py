18768fd18116294711739d38132324838b63f32f
JoMingyu <city7310@naver.com>
2018-07-11 16:00:30 +0900

    [FIX] Token refresh 관련 오류 수정 2
    - rebase해야 함

--- Server/app/views/v2/__init__.py
+++ Server/app/views/v2/__init__.py
@@ -34,7 +34,7 @@ def auth_required(model):
                 if token and isinstance(token.owner, model):
                     g.user = token.owner
                 elif token_v2 and isinstance(token_v2.key.owner, model):
-                    g.user = token_v2.owner
+                    g.user = token_v2.key.owner
                 else:
                     abort(403)
 
