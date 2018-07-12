2d03bec3180dfe414fb19cc3870f4a9706fa2f7b
JoMingyu <city7310@naver.com>
2018-07-12 10:16:16 +0900

    [UPDATE] POST /student/auth TC 리팩토링

--- Server/tests/v2/views/student/account/t_signup.py
+++ Server/tests/v2/views/student/account/t_signup.py
@@ -5,61 +5,61 @@ from tests.v2.views import TCBase
 
 class TestStudentAccountSignup(TCBase):
     """
-    학생 회원가입 테스트
+    학생 회원가입을 테스트합니다.
     """
     def __init__(self, *args, **kwargs):
         super(TestStudentAccountSignup, self).__init__(*args, **kwargs)
 
         self.method = self.client.post
-        self.target_uri = '/student/auth'
+        self.target_uri = '/student/signup'
 
-        self.new_student_id = 'new_student'
-        self.new_student_number = 2210
-        self.new_student_name = 'student2'
-        self.new_student_uuid = 'aaaa'
+        self.new_student_data = {
+            'uuid': 'aaaa',
+            'id': 'new_student',
+            'pw': self.pw,
+            'name': 'DMS',
+            'number': 1111
+        }
 
     def setUp(self):
         super(TestStudentAccountSignup, self).setUp()
 
+        # ---
+
         SignupWaitingModel(
-            number=self.new_student_number,
-            name=self.new_student_name,
-            uuid=self.new_student_uuid
+            uuid=self.new_student_data['uuid'],
+            name=self.new_student_data['name'],
+            number=self.new_student_data['number']
         ).save()
 
-        self._request = lambda *, id=self.new_student_id, pw=self.pw, uuid=self.new_student_uuid: self.request(
+        self._request = lambda *, uuid=self.new_student_data['uuid'], id=self.new_student_data['id'], pw=self.new_student_data['pw']: self.request(
             self.method,
             self.target_uri,
             json={
+                'uuid': uuid,
                 'id': id,
-                'password': pw,
-                'uuid': uuid
+                'password': pw
             }
         )
 
-        # ---
-
-    def tearDown(self):
-        StudentModel.objects(id=self.new_student_id).delete()
-        SignupWaitingModel.objects(id=self.new_student_id).delete()
-
     def testSignupSuccess(self):
-        # (1) 회원 가입
+        # (1) 회원가입
         resp = self._request()
 
         # (2) status code 201
         self.assertEqual(resp.status_code, 201)
 
-        # (3) check database StudentModel
-        student = StudentModel.objects(id=self.new_student_id)
-        self.assertIsNotNone(student)
+        # (3) 데이터베이스 확인
+        student = StudentModel.objects(
+            id=self.new_student_data['id'],
+            name=self.new_student_data['name'],
+            number=self.new_student_data['number']
+        ).first()
+        # 조건을 고의적으로 많이 달아서, 별도의 assertion 코드를 줄임
 
-        self.assertEqual(student.name, self.new_student_name)
-        self.assertEqual(student.number, self.new_student_number)
-
-        # (4) check database SignupWaitingModel
-        student = SignupWaitingModel.objects(uuid=self.new_student_uuid)
-        self.assertIsNone(student)
+        self.assertTrue(student)
+        self.assertTrue(check_password_hash(student.pw, self.new_student_data['pw']))
+        self.assertFalse(SignupWaitingModel.objects(uuid=self.new_student_data['uuid']))
 
     def testSignupFailure_idAlreadyExist(self):
         # (1) 회원가입
@@ -68,25 +68,9 @@ class TestStudentAccountSignup(TCBase):
         # (2) status code 204
         self.assertEqual(resp.status_code, 409)
 
-        # (3) check database StudentModel
-        student = StudentModel.objects(number=self.new_student_number)
-        self.assertIsNone(student)
-
-        # (4) check database SignupWaitingModel
-        student = SignupWaitingModel.objects(uuid=self.new_student_uuid)
-        self.assertIsNotNone(student)
-
     def testSignupFailure_uuidDoesntExist(self):
         # (1) 회원가입
-        resp = self._request(uuid='0000')
+        resp = self._request(uuid=self.new_student_data['uuid'] + '12345')
 
         # (2) status code 204
-        self.assertEqual(resp.status_code, 205)
-
-        # (3) check database StudentModel
-        student = StudentModel.objects(number=self.new_student_number)
-        self.assertIsNone(student)
-
-        # (4) check database SignupWaitingModel
-        student = SignupWaitingModel.objects(uuid=self.new_student_uuid)
-        self.assertIsNotNone(student)
+        self.assertEqual(resp.status_code, 204)
