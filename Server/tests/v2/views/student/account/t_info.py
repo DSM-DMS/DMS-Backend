ecac9746b7d6b176963ef70da776fab8ffdf5f4b
JoMingyu <city7310@naver.com>
2018-07-12 10:15:56 +0900

    [DONE] GET /student/info/point-history TC

--- Server/tests/v2/views/student/account/t_info.py
+++ Server/tests/v2/views/student/account/t_info.py
@@ -149,10 +149,79 @@ class TestMypage(TCBase):
         # (1) 403 체크
         self.assertEqual(self._request(token=self.admin_access_token).status_code, 403)
 
+
+class TestPointHistory(TCBase):
+    """
+    상벌점 내역 조회를 테스트합니다.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TestPointHistory, self).__init__(*args, **kwargs)
+
+        self.method = self.client.get
+        self.target_uri = '/student/info/point-history'
+
+    def setUp(self):
+        super(TestPointHistory, self).setUp()
+
         # ---
 
-        super(TestStudentAccountAuth, self).tearDown()
+        self._request = lambda *, token=None: self.request(
+            self.method,
+            self.target_uri,
+            token
+        )
 
-    def test(self):
-        # -- Test --
-        pass
+    def testDefaultStatus(self):
+        # (1) 내역 조회
+        resp = self._request()
+
+        # (2) status code 200
+        self.assertEqual(resp.status_code, 200)
+
+        # (3) response data
+        self.assertListEqual(resp.json, [])
+
+    def testAfterDataChange(self):
+        from tests.v2.views.admin.point import add_point_rules
+        good_point_rule, bad_point_rule = add_point_rules()
+
+        self.student.point_histories.append(PointHistoryModel(
+            reason=good_point_rule.name,
+            point_type=good_point_rule.point_type,
+            point=good_point_rule.min_point
+        ))
+
+        self.student.point_histories.append(PointHistoryModel(
+            reason=bad_point_rule.name,
+            point_type=bad_point_rule.point_type,
+            point=bad_point_rule.min_point
+        ))
+
+        self.student.save()
+
+        # (1) 내역 조회
+        resp = self._request()
+
+        # (2) status code 200
+        self.assertEqual(resp.status_code, 200)
+
+        # (3) response data
+        self.assertListEqual(resp.json, [
+            {
+                'point': good_point_rule.min_point,
+                'pointType': good_point_rule.point_type,
+                'reason': good_point_rule.name,
+                'time': self.today
+            },
+            {
+                'point': bad_point_rule.min_point,
+                'pointType': bad_point_rule.point_type,
+                'reason': bad_point_rule.name,
+                'time': self.today
+            }
+        ])
+
+    def testForbidden(self):
+        # (1) 403 체크
+        self.assertEqual(self._request(token=self.admin_access_token).status_code, 403)
