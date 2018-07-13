c2bee798c470a4db1e9190cd2089bb0ad077a3c4
JoMingyu <city7310@naver.com>
2018-07-13 13:12:45 +0900

    [UPDATE] 버그 신고 시 slacker가 테스트 중일때도 실행되도록 수정

--- Server/app/views/v2/student/report/bug_report.py
+++ Server/app/views/v2/student/report/bug_report.py
@@ -35,15 +35,14 @@ class BugReport(BaseResource):
         """
         payload = request.json
 
-        if not current_app.testing:
-            self.slack_bot.chat.post_message(
-                channel='#bug-report{}'.format('-for-test' if current_app.testing else ''),
-                text='제보자: {}\n제보시간: {}\n플랫폼: {}\n내용: {}'.format(
-                    g.user.name,
-                    datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    self.PLATFORM_TYPES[payload['platform']],
-                    payload['content']
-                )
+        self.slack_bot.chat.post_message(
+            channel='#bug-report{}'.format('-for-test' if current_app.testing else ''),
+            text='제보자: {}\n제보시간: {}\n플랫폼: {}\n내용: {}'.format(
+                g.user.name,
+                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                self.PLATFORM_TYPES[payload['platform']],
+                payload['content']
             )
+        )
 
         return Response('', 201)
